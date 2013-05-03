92dc3f14c30308f2c4084de2aed0930b6cb89ca3
David Barri <japgolly@gmail.com>
2013-05-03 17:40:14 +1000

    More tests for insertStep()

--- Code/usecase/src/test/scala/com/beardedlogic/usecase/lib/StepTreeTest.scala
+++ Code/usecase/src/test/scala/com/beardedlogic/usecase/lib/StepTreeTest.scala
@@ -4,7 +4,54 @@ import StepTree.{ Step, StepNode, flattenNodes, incrementPosition, insertStep }
 import org.scalatest.WordSpec
 import org.scalatest.matchers.ShouldMatchers
 
-class StepTreeTest extends WordSpec with ShouldMatchers {
+trait NodeUtils {
+
+  /**
+   * Recursively sets all IDs to null.
+   */
+  def removeIds(l: List[StepNode]): List[StepNode] = l.map((n) => n.copy(id = null, children = removeIds(n.children)))
+
+  case class NC(val node:String, val children:List[NC])
+  def $(nodes: NC*) = nodes.toList
+  implicit def nodeWithoutChildren(n:String) = NC(n,Nil)
+  implicit class StringAsNode(val s:String) { def ~>(children: List[NC]) = NC(s,children) }
+  implicit class NCListExt(val ncs: List[NC]) {
+    val regex="""^(\S+?)/(\S+)$""".r
+    def toStepNodes:List[StepNode] = toStepNodes(0,"",true)
+    def toStepNodesN:List[StepNode] = toStepNodes(0,"",false)
+    def toStepNodes(lvl:Int, idPrefix:String, genIds:Boolean):List[StepNode] = ncs.map{ nc =>
+      val (lbl,txt) = if (regex.pattern.matcher(nc.node).matches) {
+                        val regex(l,t) = nc.node; (l,t)
+                      } else
+                        (nc.node, "Step:"+nc.node)
+      val id = idPrefix + lbl
+      val ch = nc.children.toStepNodes(lvl + 1, id + ".", genIds)
+      StepNode(if (genIds) id else null, lvl, lbl, Step(txt), ch)
+    }
+  }
+
+  def inspectTree(tree:List[StepNode], indent:String="", res:List[String]=Nil):List[String] = tree match {
+    case Nil => res
+    case h :: t =>
+      val s = s"${indent}${h.label}. ${h.step.text}"
+      val ch = inspectTree(h.children, indent+"  ")
+      inspectTree(t, indent, res ::: s :: ch)
+  }
+
+  def printTrees(title1:String, nodes1:List[StepNode], title2:String, nodes2:List[StepNode]) {
+    val t1 = inspectTree(nodes1).toIndexedSeq
+    val t2 = inspectTree(nodes2).toIndexedSeq
+    val t1Size = t1 map(_.length) max
+    val fmt = s"%-${t1Size}s | %s\n"
+    val size = Vector(t1.size,t2.size).max
+    def x(l:IndexedSeq[String],i:Int) = if (i>=l.size) "" else l(i)
+    printf(fmt, title1, title2)
+    println("-"*t1Size + "-+-" + "-"*(t2 map(_.length) max))
+    for (i <- 0 until size) printf(fmt, x(t1,i), x(t2,i))
+  }
+}
+
+class StepTreeTest extends WordSpec with ShouldMatchers with NodeUtils {
   import StepTree._
 
   /**
@@ -30,35 +77,18 @@ class StepTreeTest extends WordSpec with ShouldMatchers {
    * Step test data.
    */
   object Steps {
-    val A = Step("A*")
-    val A1 = Step("A.1")
-    val A2 = Step("A.2")
-    val A3 = Step("A.3")
-    val A4 = Step("A.4")
-    val B = Step("B*")
-    val C = Step("C*")
+    val InitialTree = $("1.0" ~> $("1")).toStepNodes
+    val BigTree = $(
+        "1.0" ~> $(
+          "1",
+          "2" ~> $("a" ~> $("i","ii","iii"), "b", "c" ~> $("i","ii")),
+          "3" ~> $("a" ~> $("i"), "b"),
+          "4"),
+        "2.0" ~> $("1","2","3"),
+        "2.1" ~> $("1","2")
+      ).toStepNodes
     val N = Step("N")
-
-    val `1.0 & 1.0.1` = StepNode("id.1.0", 0, "1.0", A, StepNode("id.1.0.1", 1, "1", B, Nil) :: Nil) :: Nil
-    val `1.0.[1234]` = nodeRow(1, "1.0.", (1, A1), (2, A2), (3, A3), (4, A4))
-    val `1.0 & 1.0.[1234]` = StepNode("id.1.0", 0, "1.0", A, `1.0.[1234]`) :: Nil
   }
-
-  /**
-   * Builds a row of nodes.
-   */
-  def nodeRow(lvl: Int,
-              idPrefix: String,
-              nodes: Tuple2[Any, Step]*): List[StepNode] =
-    List((
-      for ((lbl, step) <- nodes)
-        yield StepNode(if (idPrefix == null) null else idPrefix + lbl.toString, lvl, lbl.toString, step, Nil)
-    ): _*)
-
-  /**
-   * Recursively sets all IDs to null.
-   */
-  def removeIds(l: List[StepNode]): List[StepNode] = l.map((n) => n.copy(id = null, children = removeIds(n.children)))
 
   // -------------------------------------------------------------------------------------------------------------------
 
@@ -116,55 +146,110 @@ class StepTreeTest extends WordSpec with ShouldMatchers {
     }
   }
 
-  /*
-   * TODO
-   * #
-   * |-- 1.0
-   *     |-- 1.0.1
-   *     |-- 1.0.2
-   *         | -- 1.0.2.a
-   *              | -- 1.0.2.a.i
-   *              | -- 1.0.2.a.ii
-   *              | -- 1.0.2.a.iii
-   *         | -- 1.0.2.b
-   *         | -- 1.0.2.c
-   *     |-- 1.0.3
-   *         | -- 1.0.3.a
-   *              | -- 1.0.3.a.i
-   *              | -- 1.0.3.a.ii
-   *              | -- 1.0.3.a.iii
-   *         | -- 1.0.3.b
-   *         | -- 1.0.3.c
-   *              | -- 1.0.3.c.i
-   *              | -- 1.0.3.c.ii
-   *              | -- 1.0.3.c.iii
-   *         | -- 1.0.3.d
-   *     |-- 1.0.4 
-   * |-- 2.0
-   *     |-- 2.0.1
-   *     |-- 2.0.2
-   *     |-- 2.0.3
-   * |-- 2.1
-   *     |-- 2.1.1
-   *     |-- 2.1.2
-   * 
-   */
+  // ------------------------------------------------------------------------------------------------------------------
 
   "insertStep()" when {
     import Steps._
-
-    "tree is 1.0 & 1.0.1" should {
-      "insert before 1.0.1" in {
-        removeIds(insertStep(N, "id.1.0", `1.0 & 1.0.1`)._1) should be(
-          StepNode(null, 0, "1.0", A, nodeRow(1, null, (1, N), (2, B))) :: Nil
-        )
-      }
-      "insert after 1.0.1" in {
-        removeIds(insertStep(N, "id.1.0.1", `1.0 & 1.0.1`)._1) should be(
-          StepNode(null, 0, "1.0", A, nodeRow(1, null, (1, B), (2, N))) :: Nil
-        )
+    val test = (afterId: String, nodes: List[StepNode], expected: List[NC]) => {
+      val r = insertStep(N, afterId, nodes)
+      val actual = removeIds(r._1)
+      try { actual should be (expected.toStepNodesN) }
+      catch { case e:Throwable =>
+        printTrees("EXPECTED", expected.toStepNodesN, "ACTUAL", actual)
+        throw e
       }
     }
-  }
 
+    "tree is in initial state (1.0 & 1.0.1)" should {
+      "insert before 1.0.1" in {
+        test("1.0", InitialTree, $("1.0" ~> $("1/N","2/Step:1")) )
+      }
+
+      "insert after 1.0.1" in {
+        test("1.0.1", InitialTree, $("1.0" ~> $("1","2/N")) )
+      }
+    }
+
+    "tree is large and deep" when {
+
+      "inserting after 1.0 (lvl 0) should create 1.0.1" in {
+        test("1.0", BigTree, $(
+            "1.0" ~> $(
+              "1/N",
+              "2/Step:1",
+              "3/Step:2" ~> $("a" ~> $("i","ii","iii"), "b", "c" ~> $("i","ii")),
+              "4/Step:3" ~> $("a" ~> $("i"), "b"),
+              "5/Step:4"),
+            "2.0" ~> $("1","2","3"),
+            "2.1" ~> $("1","2")
+          ))}
+
+      "inserting after 2.0 (lvl 0) should create 2.0.1" in {
+        test("2.0", BigTree, $(
+            "1.0" ~> $(
+              "1",
+              "2" ~> $("a" ~> $("i","ii","iii"), "b", "c" ~> $("i","ii")),
+              "3" ~> $("a" ~> $("i"), "b"),
+              "4"),
+            "2.0" ~> $("1/N","2/Step:1","3/Step:2","4/Step:3"),
+            "2.1" ~> $("1","2")
+          ))}
+
+      "inserting after 1.0.2 (lvl 1) should create 1.0.3" in {
+        test("1.0.2", BigTree, $(
+            "1.0" ~> $(
+              "1",
+              "2" ~> $("a" ~> $("i","ii","iii"), "b", "c" ~> $("i","ii")),
+              "3/N",
+              "4/Step:3" ~> $("a" ~> $("i"), "b"),
+              "5/Step:4"),
+            "2.0" ~> $("1","2","3"),
+            "2.1" ~> $("1","2")
+          ))}
+
+      "inserting after 2.0.2 (lvl 1) should create 2.0.3" in {
+        test("2.0.2", BigTree, $(
+            "1.0" ~> $(
+              "1",
+              "2" ~> $("a" ~> $("i","ii","iii"), "b", "c" ~> $("i","ii")),
+              "3" ~> $("a" ~> $("i"), "b"),
+              "4"),
+            "2.0" ~> $("1","2","3/N","4/Step:3"),
+            "2.1" ~> $("1","2")
+          ))}
+
+      "inserting after 1.0.2.a (lvl 2) should create 1.0.2.b" in {
+        test("1.0.2.a", BigTree, $(
+            "1.0" ~> $(
+              "1",
+              "2" ~> $("a" ~> $("i","ii","iii"), "b/N", "c/Step:b", "d/Step:c" ~> $("i","ii")),
+              "3" ~> $("a" ~> $("i"), "b"),
+              "4"),
+            "2.0" ~> $("1","2","3"),
+            "2.1" ~> $("1","2")
+          ))}
+
+      "inserting after 1.0.2.a.i (lvl 3) should create 1.0.2.a.ii" in {
+        test("1.0.2.a.i", BigTree, $(
+            "1.0" ~> $(
+              "1",
+              "2" ~> $("a" ~> $("i","ii/N", "iii/Step:ii","iv/Step:iii"), "b", "c" ~> $("i","ii")),
+              "3" ~> $("a" ~> $("i"), "b"),
+              "4"),
+            "2.0" ~> $("1","2","3"),
+            "2.1" ~> $("1","2")
+          ))}
+
+      "inserting after 1.0.2.c.ii (lvl 3) should create 1.0.2.c.iii" in {
+        test("1.0.2.c.ii", BigTree, $(
+            "1.0" ~> $(
+              "1",
+              "2" ~> $("a" ~> $("i","ii","iii"), "b", "c" ~> $("i","ii", "iii/N")),
+              "3" ~> $("a" ~> $("i"), "b"),
+              "4"),
+            "2.0" ~> $("1","2","3"),
+            "2.1" ~> $("1","2")
+          ))}
+    }
+  }
 }
