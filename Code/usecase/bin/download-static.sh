258013f6ca5cef4b9fa2733bb1404acb27787cee
David Barri <japgolly@gmail.com>
2013-06-12 11:29:15 +1000

    Added knockout.js and used it in list.html

--- Code/usecase/bin/download-static.sh
+++ Code/usecase/bin/download-static.sh
@@ -2,5 +2,9 @@
 
 v=2.1.2
 echo "Downloading v$v of normalize.css -- http://necolas.github.io/normalize.css/"
-curl http://necolas.github.io/normalize.css/$v/normalize.css -o src/main/webapp/assets/normalize.css
+curl -s http://necolas.github.io/normalize.css/$v/normalize.css -o src/main/webapp/assets/normalize.css
+
+v=2.2.1
+echo "Downloading v$v of knockout.js -- http://knockoutjs.com/"
+curl -s http://knockoutjs.com/downloads/knockout-$v.js -o src/main/webapp/assets/knockout.js
 
