e9335be81eb1cb089b29fd26a29f5398a6ddbb4e
David Barri <japgolly@gmail.com>
2013-07-31 22:04:20 +1000

    +bin/download-most.sh

--- Code/usecase/bin/download-autosize.js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-o=src/main/javascript/vendor/jquery-autosize.js
-curl https://raw.github.com/jackmoore/autosize/master/jquery.autosize.js -o $o \
-  && dos2unix $o
---
+++ Code/usecase/bin/download-most.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+v=1.10.2
+echo "Downloading v$v of jQuery -- http://jquery.com/download/"
+curl -s http://code.jquery.com/jquery-$v.min.js -o src/main/webapp/js/vendor/jquery.js
+echo $?
+
+echo "Downloading jQuery.autosize"
+o=src/main/javascript/vendor/jquery-autosize.js
+curl -s https://raw.github.com/jackmoore/autosize/master/jquery.autosize.js -o $o && dos2unix $o 2>/dev/null
+echo $?
+
+echo "Downloading jQuery.timeago"
+curl -s http://timeago.yarp.com/jquery.timeago.js -o src/main/javascript/vendor/jquery-timeago.js
+echo $?
+
+v=2.0.2
+echo "Downloading v$v of jQuery.serializeObject -- http://plugins.jquery.com/serializeObject/"
+curl -s https://raw.github.com/hongymagic/jQuery.serializeObject/$v/jquery.serializeObject.js -o src/main/javascript/vendor/jquery-serializeObject.js
+echo $?
+
+v=1.3.4
+echo "Downloading v$v of jQuery.liveQuery -- http://plugins.jquery.com/livequery/"
+curl -s https://raw.github.com/hazzik/livequery/$v/jquery.livequery.min.js -o src/main/javascript/vendor/jquery-livequery.js
+echo $?
+
+v=2.2.1
+echo "Downloading v$v of Knockout.js -- http://knockoutjs.com/"
+curl -s http://knockoutjs.com/downloads/knockout-$v.js -o src/main/javascript/vendor/knockout.js
+echo $?
+
+v=2.4.1
+echo "Downloading v$v of KO mapping -- https://github.com/SteveSanderson/knockout.mapping/tree/master/build/output"
+curl -s https://raw.github.com/SteveSanderson/knockout.mapping/$v/build/output/knockout.mapping-latest.js -o src/main/javascript/vendor/knockout-mapping.js
+echo $?
+
+v=1.12.0
+echo "Downloading v$v of QUnit -- http://qunitjs.com/"
+curl -s http://code.jquery.com/qunit/qunit-$v.js -o vendor/qunit/qunit.js && \
+curl -s http://code.jquery.com/qunit/qunit-$v.css -o vendor/qunit/qunit.css
+echo $?
+
