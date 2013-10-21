74b9c017b221dde54911c051936047e12660beff
David Barri <japgolly@gmail.com>
2013-10-22 08:50:37 +1100

    ReadUcs page now handles case where 0 UCs

--- Code/usecase/src/main/scala/com/beardedlogic/usecase/snippet/ReadOwnUcs.scala
+++ Code/usecase/src/main/scala/com/beardedlogic/usecase/snippet/ReadOwnUcs.scala
@@ -1,15 +1,17 @@
 package com.beardedlogic.usecase.snippet
 
-import com.beardedlogic.usecase.app.{RequestVars, DI}
+import net.liftweb.util.Helpers._
+import net.liftweb.util.CssSel
+import com.beardedlogic.usecase.app.{AppSiteMap, RequestVars, DI}
 import com.beardedlogic.usecase.feature.uc.{UseCase, UseCasePersistence}
 import com.beardedlogic.usecase.lib.Locks
 import com.beardedlogic.usecase.lib.Types._
 import com.beardedlogic.usecase.feature.publish.{Input, HtmlPublisher}
-import xml.NodeSeq
+import AppSiteMap.Implicits._
 
 object ReadOwnUcs {
 
-  def render = {
+  def render: CssSel = {
     val project = RequestVars.Project.get.value
 
     val ucs: List[UseCase] =
@@ -17,8 +19,12 @@ object ReadOwnUcs {
         Locks.UseCaseNumbers.read(project)(lock =>
           UseCasePersistence.loadAll(project, dao, lock)))
 
-    val i = Input(None, ucs)
-    val o: NodeSeq = HtmlPublisher.publish(i)
-    o
+    if (ucs.isEmpty)
+      "a [href]" #> AppSiteMap.Project.relativeUrl(project)
+    else {
+      val i = Input(None, ucs)
+      val o = HtmlPublisher.publish(i)
+      "* *" #> o
+    }
   }
 }
