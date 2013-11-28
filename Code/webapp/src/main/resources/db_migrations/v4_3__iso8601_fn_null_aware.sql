fc68b2cee967b262d53b54e65aa5ba1415f40758
David Barri <japgolly@gmail.com>
2013-11-28 14:54:30 +1100

    Fix: Amazon's PostgreSQL instances don't have libxml installed.

--- Code/webapp/src/main/resources/db_migrations/v4_3__iso8601_fn_null_aware.sql
+++ Code/webapp/src/main/resources/db_migrations/v4_3__iso8601_fn_null_aware.sql
@@ -1,5 +1,5 @@
 create or replace function to_iso8601_str(timestamp with time zone) returns varchar as $$
-  select case when $1 is null then null else substring(xmlelement(name x, $1)::varchar from 4 for 32) end
+  select 'obsolete, commented out due to libxml dependency'::varchar --case when $1 is null then null else substring(xmlelement(name x, $1)::varchar from 4 for 32) end
 $$ language sql immutable;
 
 -- select to_iso8601_str(now()), to_iso8601_str(NULL);
