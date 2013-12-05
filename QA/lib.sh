f9e0b3aea5241852222aefe060d4f9eed78fca0d
David Barri <japgolly@gmail.com>
2013-12-05 14:45:52 +1100

    QA: Fixed lib.sh to find url files when run from different dir.

--- QA/lib.sh
+++ QA/lib.sh
@@ -2,6 +2,8 @@
 function eval_url {
   url="$1"
   [ ! -e "$url" ] && [ -e "url-$url" ] && url="url-$url"
+  [ ! -e "$url" ] && [ -e "$(dirname "$0")/$url" ] && url="$(dirname "$0")/$url"
+  [ ! -e "$url" ] && [ -e "$(dirname "$0")/url-$url" ] && url="$(dirname "$0")/url-$url"
   [ -e "$url" ] && url="$(cat "$url" | sed '/^ *$/d' | head -1)"
 }
 
