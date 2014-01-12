8e5a153ac7dc7cccb1c51ea43de831df1334b804
David Barri <japgolly@gmail.com>
2014-01-12 14:52:29 +1100

    Customised LiftRules.supplimentalHeaders

--- Code/webapp/src/main/scala/bootstrap/liftweb/Boot.scala
+++ Code/webapp/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -4,6 +4,7 @@ import javax.mail.{Authenticator, PasswordAuthentication}
 import net.liftweb.common.Logger
 import net.liftweb.http._
 import net.liftweb.util.{Props, Mailer}
+import provider.HTTPParam
 
 import com.beardedlogic.shipreq._
 import app.{Defaults, AppSiteMap}
@@ -49,7 +50,11 @@ class Boot {
     LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
 
     // Force requests to be UTF-8
-    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+    LiftRules.early.append(_ setCharacterEncoding "UTF-8")
+
+    // Common headers: Remove X-Lift-Version, add X-Frame-Options
+    val suppHeaderList: List[HTTPParam] = List(HTTPParam("X-Frame-Options", "DENY"))
+    LiftRules.supplimentalHeaders = _ addHeaders suppHeaderList
 
     // Add support for HAML/Jade template (must be after other LiftRules)
     ScamlJade.init(List("scaml", "html"))
