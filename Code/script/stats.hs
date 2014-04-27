8e36eccd7d29b9b103f9c653ccd26b62f750f952
David Barri <japgolly@gmail.com>
2014-04-27 16:17:04 +1000

    script/stats.hs now compares logic to impl

--- Code/script/stats.hs
+++ Code/script/stats.hs
@@ -2,6 +2,7 @@
 
 import Control.Applicative
 import Control.Monad
+import Data.Graph.Inductive.Query.Monad (mapFst)
 import Data.List
 import Data.Maybe
 import Data.Monoid
@@ -50,7 +51,7 @@ type Group = String
 type Module = String
 type Stats = (Stat,Stat)
 
-data GroupD = GroupD Group [(Module, Stats)] deriving (Show)
+data GroupD = GroupD { gname :: Group, modstats :: [(Module, Stats)] } deriving (Show)
 
 data Stat = Stat { files :: Int, loc :: Int } deriving (Show, Eq)
 emptyStat = Stat 0 0
@@ -92,8 +93,9 @@ gatherAllStats = do dirs <- dirsIn "."
 ------------------------------------------------------------------------------------------------------------------------
 -- Printing stats
 
-header = "                         |       Files     |            LoC\n"
-       ++"                         |    M    T    ∑  |      M      T      ∑  (T:M)\n"
+header = "                      |       Files     |            LoC\n"
+       ++"                      |    M    T    ∑  |      M      T      ∑  (T:M)\n"
+sepLine= "----------------------+-----------------+----------------------------\n"
 
 float i = fromIntegral i :: Float
 
@@ -102,9 +104,8 @@ testRatio (Stat _ m, Stat _ t) = float t / float m
 testRatioS (Stat _ 0, _) = " - "
 testRatioS s = printf "%.1f" $ testRatio s
 
-fmtG :: GroupD -> [String] -- row per module
-fmtG (GroupD _ ms) = map fmtG' ms
-fmtG' (m,s) = printf "%-24s | %s  | %s  (%s)" m (fmtPF s) (fmtPL s) (testRatioS s)
+fmtGroup (GroupD _ ms) = map fmtMS ms
+fmtMS (m,s) = printf "%-21s | %s  | %s  (%s)\n" m (fmtPF s) (fmtPL s) (testRatioS s)
 
 fmtP :: String -> (Stat -> Int) -> Stats -> String
 fmtP prec f (a,b) =
@@ -116,7 +117,7 @@ fmtP prec f (a,b) =
 fmtPF = fmtP "4" files
 fmtPL = fmtP "6" loc
 
-fmtGroups = (unlines . fmtG =<<)
+fmtGroups = (concat . fmtGroup =<<)
 
 groupStats (GroupD _ ms) = mconcat $ map snd ms
 
@@ -124,17 +125,49 @@ singleModuleGroup name stats = GroupD name [(name,stats)]
 
 consolidateGroup gd @ (GroupD g _) = singleModuleGroup g $ groupStats gd
 
-projectTotalStats gs = mconcat (map groupStats gs)
-projectTotal = singleModuleGroup "∑" . projectTotalStats
+totalStatsForGroups gs = mconcat (map groupStats gs)
+consolidateGroups = singleModuleGroup "∑" . totalStatsForGroups
+
+fmtBreakdowns = intercalate sepLine
+
+-- Logic vs Impl
+
+headerIL = "Logic & Impl          |    L    I    ∑  |      L      I      ∑  (I:L)\n"
+
+modulesWithSuffix suf = filter (isSuffixOf suf . fst) . concatMap modstats
+
+dropr n = reverse . drop n . reverse
+
+modulesWithSuffix' suf = map (mapFst $ dropr $ length suf) . modulesWithSuffix suf
+
+logicAndImpl gs = let l = modulesWithSuffix' "-logic" gs
+                      i = modulesWithSuffix' "-impl" gs
+                      keys = sort $ nub $ map fst $ l ++ i
+                      get xs k = s where Just (_,(s,_)) = find ((== k) . fst) xs
+                      getLI k = singleModuleGroup k (get l k, get i k)
+                  in  map getLI keys
+
+logicAndImplWithTotal gs = a ++ [consolidateGroups a] where a = logicAndImpl gs
 
 ------------------------------------------------------------------------------------------------------------------------
 
+sampleData = [GroupD "base" [("base-db",(Stat {files = 5, loc = 287},Stat {files = 0, loc = 0})),("base-test",(Stat {files = 0, loc = 0},Stat {files = 3, loc = 115})),("base-util",(Stat {files = 12, loc = 769},Stat {files = 3, loc = 244}))],GroupD "taskman" [("taskman-api-impl",(Stat {files = 3, loc = 103},Stat {files = 4, loc = 72})),("taskman-api-logic",(Stat {files = 7, loc = 136},Stat {files = 2, loc = 95})),("taskman-server-impl",(Stat {files = 15, loc = 959},Stat {files = 5, loc = 408})),("taskman-server-logic",(Stat {files = 11, loc = 587},Stat {files = 5, loc = 413}))],GroupD "webapp" [("webapp",(Stat {files = 133, loc = 8388},Stat {files = 70, loc = 8239}))]]
+
 main :: IO ()
 main = do putStrLn "Analysing..."
-          gs <- gatherAllStats -- [GroupD]
+          -- gs <- pure sampleData
+          gs <- gatherAllStats
           -- putStrLn $ show gs
-          putStrLn header
-          putStrLn $ fmtGroups [projectTotal gs]
-          putStrLn $ fmtGroups $ map consolidateGroup gs
-          putStrLn $ fmtGroups gs
+          putStrLn $ fmtBreakdowns [
+            header
+            ,fmtGroups gs
+            ,fmtGroups $ map consolidateGroup gs
+            ,fmtGroups [consolidateGroups gs]
+            ]
+          putStrLn "\n"
+          putStrLn $ fmtBreakdowns [
+            headerIL
+            ,fmtGroups $ logicAndImpl gs
+            ,fmtGroups [consolidateGroups $ logicAndImpl gs]
+            ]
 
