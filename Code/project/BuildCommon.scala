73173f18ad54dcec546fe00db4a52b4af4a26a6e
David Barri <japgolly@gmail.com>
2014-06-16 15:45:23 +1000

    Added scalac flag: -Xdivergence211

--- Code/project/BuildCommon.scala
+++ Code/project/BuildCommon.scala
@@ -25,6 +25,7 @@ object Common {
   def scalacFlags = Seq(
     "-unchecked",
     "-deprecation",
+    "-Xdivergence211",
     // "-Yno-generic-signatures", // Stuffs up json4s
     "-feature", "-language:postfixOps", "-language:implicitConversions", "-language:higherKinds", "-language:existentials")
 
