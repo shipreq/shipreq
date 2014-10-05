e50138d2d4847c38a72410f880b6768df030e49f
David Barri <japgolly@gmail.com>
2014-10-05 15:37:30 +1100

    TableTest now actually confirms failureIOs commute

--- Code/webapp-client/src/test/scala/shipreq/webapp/client/ui/table/TableTest.scala
+++ Code/webapp-client/src/test/scala/shipreq/webapp/client/ui/table/TableTest.scala
@@ -2,7 +2,7 @@ package shipreq.webapp.client.ui
 package table
 
 import japgolly.scalajs.react._, vdom.ReactVDom._, all._, ScalazReact._
-import japgolly.scalajs.react.test.{Simulation, ChangeEventData, ReactTestUtils}
+import japgolly.scalajs.react.test.{Simulation, ReactTestUtils}
 import org.scalajs.dom
 import scalaz.effect.IO
 import scalaz.std.string.stringInstance
@@ -11,7 +11,6 @@ import utest._
 import shipreq.webapp.shared.validation.{GenericValidators, ValidatorPlus}
 import shipreq.webapp.client.protocol.FailureIO
 import ValidatorPlus.Implicits._
-
 
 object TableTest extends TestSuite {
 
@@ -31,8 +30,17 @@ object TableTest extends TestSuite {
       .saveNotNeededWhenP
 
   class Tester {
-    var i = 0
+    var fs = List.empty[FailureIO]
   }
+
+  def assertRowValues[D, P, A](spec: TableSpec[_, _, D, _, P, _, _])(f: (RowStatus, P) => A) =
+    (c: spec.CSF) => new {
+      def apply(m: (D, A)*): Unit = {
+        val actual = spec.getSaved(c).map { case (r, d, p) => d -> f(r, p)}.toMap
+        val expect = m.toMap
+        assert(actual == expect)
+      }
+    }
 
   override def tests = TestSuite {
 
@@ -40,8 +48,7 @@ object TableTest extends TestSuite {
 
       type X = Tester
       def save(x: X, o: Option[(Int, Data)], u: Data, f: FailureIO) = IO[Unit] {
-        println("yyyyyaaaaaaaaaaayyyyyyy")
-        x.i += 1
+        x.fs ::= f
       }
       val spec = prespec2.asyncSave(save)
 
@@ -57,37 +64,49 @@ object TableTest extends TestSuite {
           })
           val savedRows = spec.savedRows(T, savedRow)(_.sortBy(_._3.name))
           div(savedRows)
-        }).create
+        }).build
 
       val t = new Tester
-      val data = Map(2 -> Data("ABC", None)) //, 3 -> Data("DEF", Some("YAG")))
+      val data = Map(2 -> Data("ABC", None), 3 -> Data("DEF", Some("YAG")))
       val c = ReactTestUtils renderIntoDocument C((t, data))
 
       val r2 = refs(2)(c).get
-      println(s"R2 = $r2")
-      val xx = ReactTestUtils.findRenderedDOMComponentWithTag(r2, "input").domType[dom.HTMLInputElement]
-      val xxx = xx.getDOMNode()
-      println(s"XX = $xx | $xxx")
-      println(s"xxx.value = ${xxx.value}")
+      val i2 = ReactTestUtils.findRenderedDOMComponentWithTag(r2, "input").domType[dom.HTMLInputElement]
 
-      Simulation.changeAndBlur("nice") run xx
+      val r3 = refs(3)(c).get
+      val i3 = ReactTestUtils.findRenderedDOMComponentWithTag(r3, "input").domType[dom.HTMLInputElement]
 
-      println(s"xxx.value = ${xxx.value}")
-      println(s" TESTER = ${t.i}")
+      val initialState = c.state
 
-//      val data = Map(2 -> Data("ABC", None)) //, 3 -> Data("DEF", Some("YAG")))
-//      println(React renderComponentToStaticMarkup Comp(data))
-//
-//      val c1 = Comp.apply(data)
-//      val c = ReactTestUtils.renderIntoDocument(c1)
-//      val r = c.asInstanceOf[js.Dynamic].refs
-//      println(s"REFS: $r")
-//      val r2 = c.asInstanceOf[js.Dynamic].refs.hey
-//      println(s" REF: $r2")
-//      val i = ReactTestUtils.findRenderedDOMComponentWithTag(c, "input")
-//      println(s"I = $i")
-//      ReactTestUtils.Simulate.change(i, js.Dynamic.literal(data = "no"))
-//      println(s"I = $i")
+      def assertRowStatuses = assertRowValues(spec)((r, p) => r)(c)
+
+      def setup(): Unit = {
+        c setState initialState
+        t.fs = Nil
+        Simulation.focusChangeBlur("blar2") run i2
+        assertRowStatuses(2 -> RowStatus.Locked, 3 -> RowStatus.Sync)
+        Simulation.focusChangeBlur("blar3") run i3
+        assertRowStatuses(2 -> RowStatus.Locked, 3 -> RowStatus.Locked)
+      }
+
+      'inOrder {
+        setup()
+        val List(f3, f2) = t.fs
+        f2.io.unsafePerformIO()
+        assertRowStatuses(2 -> RowStatus.Failed, 3 -> RowStatus.Locked)
+        f3.io.unsafePerformIO()
+        assertRowStatuses(2 -> RowStatus.Failed, 3 -> RowStatus.Failed)
+      }
+
+      'outOfOrder{
+        setup()
+        val List(f3, f2) = t.fs
+        f3.io.unsafePerformIO()
+        assertRowStatuses(2 -> RowStatus.Locked, 3 -> RowStatus.Failed)
+        f2.io.unsafePerformIO()
+        assertRowStatuses(2 -> RowStatus.Failed, 3 -> RowStatus.Failed)
+      }
+
     }
   }
 }
