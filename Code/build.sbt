0a955f94f366c14401f8bb28ebccdc5b3e435b84
David Barri <japgolly@gmail.com>
2015-01-14 12:02:25 +1100

    Scala 2.11.{4 ⇒ 5}

--- Code/build.sbt
+++ Code/build.sbt
@@ -2,6 +2,3 @@ name := "ShipReq"
 
 startYear := Some(2013)
 
-initialize ~= { _ =>
-  sys.props("scalac.patmat.analysisBudget") = "off"
-}
--- Code/project/Dependencies.scala
+++ Code/project/Dependencies.scala
@@ -59,7 +59,7 @@ object Deps {
     }
   }
 
-  object Scala extends Group("2.11.4", "org.scala-lang") {
+  object Scala extends Group("2.11.5", "org.scala-lang") {
     val compiler = d("scala-compiler")
     val library  = d("scala-library")
     val reflect  = d("scala-reflect")
