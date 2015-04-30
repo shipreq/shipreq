3bf7f45e915cb63f3f50cf9232d7f0ff01dfc2b0
David Barri <japgolly@gmail.com>
2015-04-30 16:09:23 +1000

    Add keys to ReqCodeGroups in Table

--- Code/webapp-client/src/main/scala/shipreq/webapp/client/app/ui/reqtable/Table.scala
+++ Code/webapp-client/src/main/scala/shipreq/webapp/client/app/ui/reqtable/Table.scala
@@ -153,7 +153,8 @@ object Table {
 
       val uniqKey = new KeyUniqueness
       val rowKey: Row => js.Any = {
-        case r: GenericReqRow => uniqKey(r.req.id.value)
+        case r: GenericReqRow   => uniqKey(r.req.id.value)
+        case r: ReqCodeGroupRow => uniqKey(r.id.value.value)
       }
 
       def renderRows =
