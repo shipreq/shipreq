7d9aee080008a1a35fc21937b88b25a775110212
David Barri <japgolly@gmail.com>
2015-06-28 23:25:17 +1000

    Change serialisation benchmark settings

--- Code/benchmark-jvm/src/main/scala/shipreq/benchmark/Serialisation.scala
+++ Code/benchmark-jvm/src/main/scala/shipreq/benchmark/Serialisation.scala
@@ -5,8 +5,8 @@ import java.util.concurrent.TimeUnit
 import shipreq.webapp.base.protocol.DataCodecs
 
 @State(Scope.Benchmark)
-@BenchmarkMode(Array(Mode.SampleTime))
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+//@BenchmarkMode(Array(Mode.SampleTime))
+//@OutputTimeUnit(TimeUnit.MICROSECONDS)
 class Serialisation {
 
   implicit val projectCodec = DataCodecs.project
@@ -14,9 +14,9 @@ class Serialisation {
   private val p1000 = data.Project_1000.project
 
   @Benchmark
-  def serialise_100 = upickle.Fns write p100
+  def write_100 = upickle.Fns write p100
 
   @Benchmark
-  def serialise_1000 = upickle.Fns write p1000
+  def write_1000 = upickle.Fns write p1000
 
 }
