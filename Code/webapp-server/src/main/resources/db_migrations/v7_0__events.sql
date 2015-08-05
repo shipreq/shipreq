a6fe613bf45bfe10f377cee99d6c37601e3b2512
David Barri <japgolly@gmail.com>
2015-08-06 08:31:58 +1000

    event table needs data_id_type

--- Code/webapp-server/src/main/resources/db_migrations/v7_0__events.sql
+++ Code/webapp-server/src/main/resources/db_migrations/v7_0__events.sql
@@ -1,16 +1,18 @@
 CREATE TABLE event (
-  project_id  BIGINT      NOT NULL REFERENCES project,
-  seq         INTEGER     NOT NULL CHECK (seq >= 0),
-  type_id     SMALLINT    NOT NULL,
-  data_id     INTEGER     NULL,
-  data        JSON        NULL,
-  hash_scheme SMALLINT    NOT NULL CHECK (hash_scheme > 0),
-  hash        INTEGER     NOT NULL,
-  created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  project_id   BIGINT      NOT NULL REFERENCES project,
+  seq          INTEGER     NOT NULL CHECK (seq >= 0),
+  type_id      SMALLINT    NOT NULL,
+  data_id_type "char"      NOT NULL,
+  data_id      INTEGER     NULL,
+  data         JSON        NULL,
+  hash_scheme  SMALLINT    NOT NULL CHECK (hash_scheme > 0),
+  hash         INTEGER     NOT NULL,
+  created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   UNIQUE(project_id, seq));
 
-COMMENT ON TABLE  event         IS 'An append-only sequence of modification events applied to an empty project.';
-COMMENT ON COLUMN event.seq     IS 'Order in which events for a Project are to be applied.';
-COMMENT ON COLUMN event.type_id IS 'A number representing the type of event. Defined and maintained in Scala only.';
-COMMENT ON COLUMN event.data_id IS 'Meaning depends on event type. Usually the local ID of an object in the Project.';
-COMMENT ON COLUMN event.data    IS 'Meaning and format depends on event type.';
+COMMENT ON TABLE  event              IS 'An append-only sequence of modification events applied to an empty project.';
+COMMENT ON COLUMN event.seq          IS 'Order in which events for a Project are to be applied.';
+COMMENT ON COLUMN event.type_id      IS 'A number representing the type of event. Defined and maintained in Scala only.';
+COMMENT ON COLUMN event.data_id_type IS 'The type of data_id as IDs can be polymorphic.';
+COMMENT ON COLUMN event.data_id      IS 'Meaning depends on event type. Usually the local ID of an object in the Project.';
+COMMENT ON COLUMN event.data         IS 'Meaning and format depends on event type.';
