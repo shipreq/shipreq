efcd58395228bb6cf7d27df9482f377d7c6b1c8d
David Barri <japgolly@gmail.com>
2016-05-02 19:46:23 +1000

    Provide better output on GraphViz crash

--- Code/webapp-client-ww/src/main/scala/shipreq/webapp/client/ww/GraphViz.scala
+++ Code/webapp-client-ww/src/main/scala/shipreq/webapp/client/ww/GraphViz.scala
@@ -1,5 +1,6 @@
 package shipreq.webapp.client.ww
 
+import org.scalajs.dom.console
 import org.scalajs.dom.webworkers.DedicatedWorkerGlobalScope
 import scala.scalajs.js
 import shipreq.webapp.base.WebappConfig
@@ -17,11 +18,16 @@ object GraphViz {
 
   private val titlesAndComments = "(?:<title>[^<>]*?</title>|<!--[^\u0000]*?-->)".r
 
-  def apply(dot: DOT): SVG = {
-    var svg = instance(dot.content, "svg")
-    svg = titlesAndComments.replaceAllIn(svg, "")
-    SVG(svg)
-  }
+  def apply(dot: DOT): SVG =
+    try {
+      var svg = instance(dot.content, "svg")
+      svg = titlesAndComments.replaceAllIn(svg, "")
+      SVG(svg)
+    } catch {
+      case t: Throwable =>
+        console.error("GraphViz crash: ", dot.content)
+        throw t
+    }
 
   case class DOT(content: String) extends AnyVal {
     @inline def toSVG: SVG =
