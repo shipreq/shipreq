fb3e1e2d4c7d2a38b857f645e6abece9a6e03106
David Barri <japgolly@gmail.com>
2017-01-05 15:13:45 +1100

    SBT refactor: Move WebappServerBuild into WebappBuild

--- Code/project/WebappBuild.scala
+++ Code/project/WebappBuild.scala
@@ -210,7 +210,118 @@ object WebappBuild {
       .dependsOn(webappBaseTest % "test->compile")
 
   lazy val webappServer =
-    project("webapp-server").configure(WebappServerBuild.apply)
+    project("webapp-server").configure(Server.definition)
+
+  object Server {
+    import com.earldouglas.xwp._
+    import ContainerPlugin.autoImport._
+    import JettyPlugin    .autoImport._
+    import WebappPlugin   .autoImport._
+
+    lazy val copyClientJs = taskKey[Unit]("Copies required webapp client resources.")
+
+    def clientJsSettings: Project => Project =
+      _.settings(
+
+        cleanFiles ++= {
+          val webapp = (sourceDirectory in webappPrepare).value
+          def sjs(name: String, sourceMap: String): Seq[File] =
+            Seq(
+              webapp / s"dev/$name.js",
+              webapp / s"dev/$sourceMap-fastopt.js.map",
+              webapp / s"a/$name.js")
+
+          sjs("client-home"   , "webapp-client-home") ++
+          sjs("client-project", "webapp-client-project") ++
+          sjs("ww"            , "webapp-client-ww")
+        },
+
+        copyClientJs := {
+          implicit val log = streams.value.log
+          val webapp = (sourceDirectory in webappPrepare).value
+
+          def syncSJS(jsf: VirtualJSFile, name: String): Unit =
+            jsf match {
+              case f: FileVirtualJSFile =>
+                if (devMode) {
+                  fileSync(f.file         , webapp / s"dev/$name.js"                , mandatory = true)
+                  fileSync(f.sourceMapFile, webapp / "dev" / f.sourceMapFile.getName, mandatory = false)
+                  // This exact filename is specified at end of js ↑
+                } else {
+                  fileSync(f.file, webapp / s"a/$name.js", mandatory = true)
+                }
+              case other =>
+                sys.error("Unsupported virtual file type: " + other)
+            }
+
+          syncSJS((scalaJSLinkedFile in Compile in webappClientHome   ).value, "client-home")
+          syncSJS((scalaJSLinkedFile in Compile in webappClientProject).value, "client-project")
+          syncSJS((scalaJSLinkedFile in Compile in webappClientWw     ).value, "ww")
+        },
+
+        { val k = Keys.`package`; k := k.dependsOn(copyClientJs).value },
+        { val k = webappPrepare ; k := k.dependsOn(copyClientJs).value }
+      )
+
+    def warSettings = {
+      var dirHitList = Set("_scalate")
+      if (releaseMode)
+        dirHitList += "dev"
+
+      (_: Project).settings(
+
+        // Expand this the webapp-server module instead of building a jar
+        // At the minimum, scripts in Release/webapp expect to find WEB-INF/classes/build.properties
+        webappWebInfClasses := true,
+
+        // Remove dirs from the WAR
+        webappPostProcess := { webappDir =>
+          def go(f: File): Unit = {
+            if (f.isDirectory) {
+              if (dirHitList contains f.getName) {
+                streams.value.log.info(s"Deleting ${f.getAbsolutePath}")
+                IO.delete(f)
+              } else
+                f.listFiles foreach go
+            }
+          }
+          go(webappDir)
+        })
+    }
+
+    def testSettings = (_: Project)
+      .dependsOn(webappBaseTestJvm % "test->compile")
+      .settings(inConfig(Test)(Seq(
+        fork                         := true,
+        javaOptions                  += "-Drun.mode=test",
+        unmanagedResourceDirectories += baseDirectory.value / "src/main/webapp", // So templates load
+        parallelExecution            := false) // Due to UserFixture+Oshiro and LiveTest
+      ): _*)
+
+    def consoleCmds = "def initLift() = {val b = new bootstrap.liftweb.Boot; b.configureLift; b}"
+
+    def definition = (_: Project)
+      .enablePlugins(JettyPlugin, WarPlugin)
+      .dependsOn(baseDb, taskmanApi, webappBaseJvm, webappBaseServerJvm, webappGenJvm)
+      .deps(
+        Scalaz.core ++ Lift.webkit ++ Shiro.all ++ scalate ++ commonsLang ++
+        testScope(μTest ++ scalaTest ++ scalaCheck ++ Lift.testkit ++ commonsIo ++ twitterEval) ++
+        (LibJetty.webapp % "test") ++
+        (LibJetty.servletApi % "test,provided"))
+      .configure(
+        webappSettings,
+        Common.jvmSettings,
+        Common.generateBuildPropFile(),
+        clientJsSettings,
+        warSettings,
+        testSettings,
+        dontInline) // crashes scalac 2.11.7
+      .settings(
+        containerLibs in Jetty := LibJetty.runner(JVM).map(_.intransitive()),
+        javaOptions in Jetty += "-Xmx1g",
+        initialCommands += consoleCmds,
+        fullClasspath in console in Compile += file("src/main/webapp")) // So templates can be loaded from console
+  }
 
   // ===================================================================================================================
 
