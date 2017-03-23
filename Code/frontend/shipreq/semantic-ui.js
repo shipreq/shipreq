b015405ebdd22135537ea021f2379be040817ee9
David Barri <japgolly@gmail.com>
2017-03-23 17:04:36 +1100

    Add custom semantic-ui module

---
+++ Code/frontend/shipreq/semantic-ui.js
@@ -0,0 +1,4 @@
+require('../semantic-ui/semantic/dist/semantic.min.js');
+require('../semantic-ui/semantic/dist/semantic.min.css');
+require('./semantic-ui-fixes.css');
+require('./semantic-ui-textcomplete.css');
