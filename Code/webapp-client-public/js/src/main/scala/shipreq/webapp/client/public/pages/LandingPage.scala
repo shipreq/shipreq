419124bf07c597baac337fe0f81e98d10e159569
David Barri <japgolly@gmail.com>
2017-06-30 14:19:22 +1000

    Tweak LandingPage component (still WIP)

--- Code/webapp-client-public/js/src/main/scala/shipreq/webapp/client/public/pages/LandingPage.scala
+++ Code/webapp-client-public/js/src/main/scala/shipreq/webapp/client/public/pages/LandingPage.scala
@@ -3,8 +3,9 @@ package shipreq.webapp.client.public.pages
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import shipreq.webapp.base.{AssetManifest, WebappConfig}
-import shipreq.webapp.base.ui.semantic.{Button, Colour, Form, Icon, Input, Size}
+import shipreq.webapp.base.ui.semantic._
 import shipreq.webapp.client.public.Styles.{landingPage => *}
+import shipreq.webapp.client.public.protocol.LandingPageProtocol.Request
 
 object LandingPage {
 
@@ -12,41 +13,40 @@ object LandingPage {
     @inline def render = Component(this)
   }
 
+  private val header: TagMod =
+    TagMod(
+      <.div(
+        <.img(*.banner, ^.src := AssetManifest.shipreqBannerSvg, ^.alt := WebappConfig.appName)),
+      <.div(*.tagline,
+        "Ship better products with better requirements."))
+
+  private val yap = ScalaComponent.static("")(
+    <.div(*.yap,
+      <.div(*.yap1,
+        "ShipReq is a modern, online tool", <.br,
+        "for requirements development and management,", <.br,
+        "currently in private beta phase."
+      ),
+      <.div(*.yap2,
+        "Would you like to know more, or participate in the beta?", <.br,
+        "Get in touch !",
+        <.span(*.pointAtForm))))
+
   final class Backend($: BackendScope[Props, Unit]) {
     def render(p: Props): VdomElement = {
 
-      val yap =
-        <.div(*.yap,
-          <.div(*.yap1,
-            "ShipReq is a modern, online tool", <.br,
-            "for requirements development and management,", <.br,
-            "currently in private beta phase."
-          ),
-          <.div(*.yap2,
-            "Would you like to know more, or participate in the beta?", <.br,
-            "Get in touch !",
-            <.span(*.pointAtForm)))
-
       val form = {
-        val fieldName = Input.Text.icon(Icon.User.tag, <.input.text(^.placeholder := "Your name"))
-        val fieldMail = Input.Text.icon(Icon.Mail.tag, <.input.text(^.placeholder := "Your email address"))
+        val fieldName = Input.Text.icon(Icon.User.tag, <.input.text(^.placeholder := Request.labelName))
+        val fieldMail = Input.Text.icon(Icon.Mail.tag, <.input.text(^.placeholder := Request.labelEmail))
         val fieldText = <.textarea(^.rows := 12, ^.placeholder := "What would you like to say?")
         val fieldNews = Form.Field.center(Input.Checkbox(EmptyVdom, "Subscribe to newsletter"))
         val fieldSend = Form.Field.center(Button(colour = Colour.Blue, size = Size.Large).tag(*.formSubmit, "Express Interest"))
         <.div(*.formCont, <.div(*.form, Form(fieldName, fieldMail, fieldText, fieldNews, fieldSend)))
       }
 
-
-      <.div(*.cont,
-        <.div(
-          <.img(*.banner, ^.src := AssetManifest.shipreqBannerSvg, ^.alt := WebappConfig.appName)),
-        <.div(*.tagline,
-          "Ship better products with better requirements."),
-
-        <.div(*.part2,
-          yap,
-          form))
-   }
+      <.div(*.cont, header,
+        <.div(*.part2, yap(), form))
+    }
   }
 
   val Component = ScalaComponent.builder[Props]("LandingPage")
