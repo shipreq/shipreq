0d5ce1b21436e1edb21f74ba7ce65874b2d04332
David Barri <japgolly@gmail.com>
2017-07-05 17:28:15 +1000

    Optimise Url and add tests

--- Code/base-util/shared/src/main/scala/shipreq/base/util/Url.scala
+++ Code/base-util/shared/src/main/scala/shipreq/base/util/Url.scala
@@ -1,22 +1,39 @@
 package shipreq.base.util
 
+import japgolly.univeq.UnivEq
+
 object Url {
 
   // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-  /** Always starts with a slash. */
-  final case class Relative(relativeUrl: String) extends AnyVal {
+  /**
+    * @param relativeUrlNoHeadSlash The URL without a leading slash.
+    */
+  final case class Relative private[Relative](relativeUrlNoHeadSlash: String) extends AnyVal {
+    def relativeUrlNoHeadOrTailSlash: String = dropTailSlashes(relativeUrlNoHeadSlash)
+    def relativeUrlNoTailSlash      : String = "/" + relativeUrlNoHeadOrTailSlash
+    def relativeUrl                 : String = "/" + relativeUrlNoHeadSlash
+
+    def isRoot: Boolean =
+      relativeUrlNoHeadSlash.isEmpty
+
     def thenParam[A](f: A => String): Relative.Param1[A] =
-      Relative.Param1(this, f)
+      new Relative.Param1(new Relative(relativeUrlNoHeadOrTailSlash), f)
   }
+
   object Relative {
     def apply(value: String): Relative =
-      new Relative(value.replaceFirst("^/*", "/"))
+      new Relative(dropHeadSlashes(value))
+
+    implicit def univEq: UnivEq[Relative] = UnivEq.derive
 
     /** Represents `/prefix/<A>`; the param is always last */
-    final case class Param1[-A](prefix: Relative, suffix: A => String) {
-      assert(prefix.relativeUrl != "/")
+    final class Param1[-A] private[Relative](val prefix: Relative, val suffix: A => String) {
 
-      private val pre = prefix.relativeUrl + "/"
+      private val pre =
+        if (prefix.isRoot)
+          ""
+        else
+          prefix.relativeUrlNoHeadOrTailSlash + "/"
 
       def apply(a: A): Relative =
         new Relative(pre + suffix(a))
@@ -24,19 +41,28 @@ object Url {
   }
 
   // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-  final case class Absolute(absoluteUrl: String) extends AnyVal
+  final case class Absolute private[Absolute](absoluteUrl: String) extends AnyVal
+
   object Absolute {
 
-    /** Never ends with a slash. */
-    final case class Base(value: String) extends AnyVal {
+    /** An absolute URL until (and excluding) the path.
+      *
+      * @param value Never ends with a slash.
+      *              Eg. "http://qwe.com:123"
+      */
+    final case class Base private[Base](value: String) extends AnyVal {
       def /(r: Relative): Absolute =
-        Absolute(value + r.relativeUrl)
+        Absolute(if (r.isRoot) value else value + r.relativeUrl)
+
       def /[A](r: Relative.Param1[A]): Absolute.Param1[A] =
         Absolute.Param1(this / r.prefix, r.suffix)
     }
+
     object Base {
       def apply(value: String): Base =
-        new Base(value.replaceFirst("/+$", ""))
+        new Base(dropTailSlashes(value))
+
+      implicit def univEq: UnivEq[Base] = UnivEq.derive
     }
 
     /** Represents `https://blah.com/prefix/<A>`; the param is always last */
@@ -49,4 +75,20 @@ object Url {
     }
   }
 
+  // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+  /** Super-efficient version of _.dropWhile(_ == '/') */
+  val dropHeadSlashes: String => String = s => {
+    var i = 0
+    while (i < s.length && s(i) == '/') i += 1
+    if (i == 0) s else s.substring(i)
+  }
+
+  /** Super-efficient version of _.dropRightWhile(_ == '/') */
+  val dropTailSlashes: String => String = s => {
+    val j = s.length - 1
+    var i = j
+    while (i >= 0 && s(i) == '/') i -= 1
+    if (i == j) s else s.substring(0, j)
+  }
 }
---
+++ Code/base-util/shared/src/test/scala/shipreq/base/util/UrlTest.scala
@@ -0,0 +1,71 @@
+package shipreq.base.util
+
+import japgolly.microlibs.testutil.TestUtil._
+import shipreq.base.util.univeq._
+import utest._
+
+object UrlTest extends TestSuite {
+  import Url._
+
+  val googleStr = "https://google.com"
+  val google = Absolute.Base(googleStr)
+
+  override def tests = TestSuite {
+
+    'relative {
+      'nullary {
+        def test(u: Relative, noSlash: String): Unit = {
+          assertEq(u.relativeUrlNoHeadSlash, noSlash)
+          assertEq(u.relativeUrl, "/" + noSlash)
+        }
+        "/"     - test(Relative("/"), "")
+        "/x"    - test(Relative("/x"), "x")
+        "/x/y"  - test(Relative("/x/y"), "x/y")
+        "/x/y/" - test(Relative("/x/y/"), "x/y/")
+        'empty  - assertEq(Relative(""), Relative("/"))
+        'head2  - assertEq(Relative("//"), Relative("/"))
+        "x"     - assertEq(Relative("x"), Relative("/x"))
+      }
+      'unary {
+        def test(p: Relative, prefix: String): String = {
+          val u = p.thenParam[Int](_.toString)(123)
+          assertEq(u.relativeUrl, s"$prefix/123")
+          assert(!u.relativeUrlNoHeadSlash.startsWith("/"))
+          u.relativeUrl
+        }
+        "/"      - test(Relative("/"), "")
+        "/x"     - test(Relative("/x"), "/x")
+        "/x/y"   - test(Relative("/x/y"), "/x/y")
+        "/x/y/"  - test(Relative("/x/y/"), "/x/y")
+        "/x/y//" - test(Relative("/x/y//"), "/x/y")
+      }
+    }
+
+    'absoluteBase {
+      'noSlash - assertEq(Absolute.Base(googleStr).value, googleStr)
+      'slash - assertEq(Absolute.Base(googleStr + "/").value, googleStr)
+    }
+
+    'absolute {
+      'nullary {
+        def test(r: Relative, path: String): String = {
+          val a = google / r
+          assertEq(a.absoluteUrl, googleStr + path)
+          a.absoluteUrl
+        }
+        "/"  - test(Relative("/"), "")
+        "/x" - test(Relative("/x"), "/x")
+      }
+      'unary {
+        def test(r: Relative, pathPrefix: String): String = {
+          val a = google / r.thenParam[Int](_.toString)(123)
+          assertEq(a.absoluteUrl, googleStr + pathPrefix + 123)
+          a.absoluteUrl
+        }
+        "/"  - test(Relative("/"), "/")
+        "/x" - test(Relative("/x"), "/x/")
+      }
+    }
+
+  }
+}
