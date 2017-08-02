6a41ce0756264ed9d3aaa7b7687aa447d55e1f1a
David Barri <japgolly@gmail.com>
2017-08-02 10:40:10 +1000

    GCP learning and slow progress - Cluster & DB via DM
    Also increased cluster memory cos GKE adds a few services that take
    around 200MB or so

--- Deploy/infra.tf
+++ Deploy/infra.tf
@@ -4,32 +4,34 @@ provider "google" {
   region      = "australia-southeast1"
 }
 
-resource "google_compute_instance" "taskman" {
-  name         = "taskman"
-  machine_type = "f1-micro"
-  zone         = "australia-southeast1-c"
+resource "google_container_cluster" "apps" {
+  name               = "apps"
+  zone               = "australia-southeast1-b"
+  initial_node_count = 2
 
-  disk {
-    image       = "cos-cloud/cos-stable"
-    size        = 10
-    auto_delete = true
-    boot        = true
-  }
+  node_config {
+    disk_size_gb = 10
+    machine_type = "custom-2-2560"
+    image_type   = "COS"
 
-  scheduling {
-    preemptible       = true
-    automatic_restart = true
-  }
-
-  network_interface {
-    network = "default"
-  }
-
-  service_account {
-    scopes = [
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
       "https://www.googleapis.com/auth/trace.append",
     ]
+  }
+
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+
+    horizontal_pod_autoscaling {
+      disabled = true
+    }
   }
 }
