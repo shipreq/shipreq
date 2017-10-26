b5708193de4a2767dd6fb17e732a5770ed033821
David Barri <japgolly@gmail.com>
2017-10-26 22:15:56 +1100

    Add SavedView transition logic tests

--- Code/webapp-client-project/src/test/scala/shipreq/webapp/client/project/app/reqtable/SavedViewLogicTest.scala
+++ Code/webapp-client-project/src/test/scala/shipreq/webapp/client/project/app/reqtable/SavedViewLogicTest.scala
@@ -40,12 +40,21 @@ object SavedViewLogicTest extends TestSuite {
       filter     = None))
 
   val SVs = SavedViews(SVb) + SVa + SVc
+  val `SVs-A` = SavedViews(SVb) + SVc
+  val `SVs-B` = SavedViews(SVa) + SVc
+  val `SVs-C` = SavedViews(SVb) + SVa
+
+  val Dirty1 = SVa.view.copy(filter = None)
+  val Dirty2 = SVa.view.copy(filter = Some(Filter.Valid.text("WOAH!!")))
 
   implicit def autoSome[A](a: A): Option[A] =
     Some(a)
 
   implicit def savedViewToFilterDead(v: SavedView): FilterDead =
     v.view.filterDead
+
+  implicit def savedViewToSomeId(v: SavedView): Option[SavedView.Id] =
+    Some(v.id)
 
   implicit final class SavedViewTestExt(private val self: SavedView) extends AnyVal {
     def asDefault: Menu.Item.Default =
@@ -56,6 +65,50 @@ object SavedViewLogicTest extends TestSuite {
   }
 
   override def tests = TestSuite {
+
+    'actions {
+      import Action._
+
+      def test(svs: SavedViews.Optional, state: State)
+              (a: Action, svs2: SavedViews.Optional = null)
+              (expectedState: State, expectedView: View) = {
+        val s2 = Action.interpret(svs)(a)(state)
+        assertEq("Next State", s2, expectedState)
+        assertEq("Next View", s2.activeView(Option(svs2) getOrElse svs, HideDead), expectedView)
+      }
+
+      'modify {
+        'e_u - test(None, State(None, None))(Modify(Dirty1))(State(Some(Dirty1), None), Dirty1)
+        'sc_sd - test(SVs, State(None, SVa))(Modify(Dirty1))(State(Some(Dirty1), SVa), Dirty1)
+        'sd_sd - test(SVs, State(Dirty1, SVa))(Modify(Dirty2))(State(Some(Dirty2), SVa), Dirty2)
+        'sd_sc - test(SVs, State(Dirty1, SVa))(Modify(SVa.view))(State(Some(SVa.view), SVa), SVa.view)
+      }
+
+      'select {
+        'sc_sc - test(SVs, State(None, None))(Select(SVa.id))(State(None, SVa), SVa.view)
+        'sc_sc - test(SVs, State(None, SVb))(Select(SVa.id))(State(None, SVa), SVa.view)
+        'sd_sc - test(SVs, State(Dirty1, SVb))(Select(SVa.id))(State(None, SVa), SVa.view)
+      }
+
+      'delete {
+        // Three letters are:
+        // 1. [_m]  - Manual view
+        // 2. [_ab] - Ref view id
+        // 3. [ab]  - Delete
+        '__b - test(SVs, State(None  , None))(Delete(SVb.id), `SVs-B`)(State(SVb.view, None), SVb.view)
+        '__a - test(SVs, State(None  , None))(Delete(SVa.id), `SVs-A`)(State(None    , None), SVb.view)
+        '_bb - test(SVs, State(None  , SVb ))(Delete(SVb.id), `SVs-B`)(State(SVb.view, None), SVb.view)
+        '_ba - test(SVs, State(None  , SVb ))(Delete(SVa.id), `SVs-A`)(State(None    , SVb ), SVb.view)
+        '_ab - test(SVs, State(None  , SVa ))(Delete(SVb.id), `SVs-B`)(State(None    , SVa ), SVa.view)
+        '_aa - test(SVs, State(None  , SVa ))(Delete(SVa.id), `SVs-A`)(State(SVa.view, None), SVa.view)
+        'm_b - test(SVs, State(Dirty1, None))(Delete(SVb.id), `SVs-B`)(State(Dirty1  , None), Dirty1  )
+        'm_a - test(SVs, State(Dirty1, None))(Delete(SVa.id), `SVs-A`)(State(Dirty1  , None), Dirty1  )
+        'mbb - test(SVs, State(Dirty1, SVb ))(Delete(SVb.id), `SVs-B`)(State(Dirty1  , None), Dirty1  )
+        'mba - test(SVs, State(Dirty1, SVb ))(Delete(SVa.id), `SVs-A`)(State(Dirty1  , SVb ), Dirty1  )
+        'mab - test(SVs, State(Dirty1, SVa ))(Delete(SVb.id), `SVs-B`)(State(Dirty1  , SVa ), Dirty1  )
+        'maa - test(SVs, State(Dirty1, SVa ))(Delete(SVa.id), `SVs-A`)(State(Dirty1  , None), Dirty1  )
+      }
+    }
 
     'menu {
       import Menu.{determine => _, _}
