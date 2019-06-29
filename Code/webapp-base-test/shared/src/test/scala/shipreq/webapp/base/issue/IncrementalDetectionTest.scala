6afdc941521df040a6b0ebdb87f30332c8f4be7f
David Barri <japgolly@gmail.com>
2019-06-29 19:24:23 +1000

    Measure usefulness of incremental issue detection
    AVERAGE    :   init =   467 us,   inc =   392 us,   saved =      75 us
    TOTAL      :   init =  1.8 sec,   inc =  1.5 sec,   saved =     288 ms

--- Code/webapp-base-test/shared/src/test/scala/shipreq/webapp/base/issue/IncrementalDetectionTest.scala
+++ Code/webapp-base-test/shared/src/test/scala/shipreq/webapp/base/issue/IncrementalDetectionTest.scala
@@ -1,5 +1,7 @@
 package shipreq.webapp.base.issue
 
+import japgolly.microlibs.stdlib_ext.StdlibExt._
+import java.time.Duration
 import nyaya.gen.Gen
 import scala.annotation.tailrec
 import utest._
@@ -9,17 +11,38 @@ import shipreq.webapp.base.test.WebappTestUtil._
 
 object IncrementalDetectionTest extends TestSuite {
 
-  private val genEvents = RandomEventStream.justEntireEventStream(128)
+  private def eventsStreamSize = 1024
+  private def compareIncTime = true
+  private var fullDurs, incDurs = List.empty[Duration]
+
+  private def logCmpDur(desc: String, full: Duration, inc: Duration): Unit = {
+    val saved = full.minus(inc)
+    printf(s"%-10s :   init = %8s,   inc = %8s,   saved = %10s\n", desc, full.conciseDesc, inc.conciseDesc, saved.conciseDesc)
+  }
+
+  private val genEvents = RandomEventStream.justEntireEventStream(eventsStreamSize)
 
   private def assertIncrementsEqualWhole(windowSize: Int, ves: Vector[VerifiedEvent], seed: Long): Unit = {
     @tailrec
     def go(it: IssueTracker, remainingEvents: Vector[VerifiedEvent], taken: Int): Unit =
       if (remainingEvents.nonEmpty) {
-        val es     = remainingEvents.take(windowSize)
-        val p2     = applyVerifiedEventSuccessfully(it.project, es: _*)
-        val it2    = it.update(es.iterator.map(_.event), p2)
-        val taken2 = taken + windowSize
-        val expect = IssueTracker(p2)
+        val es        = remainingEvents.take(windowSize)
+        val p2        = applyVerifiedEventSuccessfully(it.project, es: _*)
+        val incStart  = System.nanoTime()
+        val it2       = it.update(es.iterator.map(_.event), p2)
+        val incEnd    = System.nanoTime()
+        val taken2    = taken + windowSize
+        val fullStart = System.nanoTime()
+        val expect    = IssueTracker(p2)
+        val fullEnd   = System.nanoTime()
+
+        if (compareIncTime) {
+          val fullDur = Duration.ofNanos(fullEnd - fullStart)
+          val incDur  = Duration.ofNanos(incEnd - incStart)
+          fullDurs  ::= fullDur
+          incDurs   ::= incDur
+          logCmpDur(s"$taken2+$windowSize", full = fullDur, inc = incDur)
+        }
 
         onFail {
           assertIssueSet(s"[windowSize=$windowSize, seed = ${seed}L , events=$taken]",
@@ -53,11 +76,38 @@ object IncrementalDetectionTest extends TestSuite {
 
       def test(seed: Long = Gen.long.sample()): Unit = {
         val ves = (Gen.setSeed(seed) >> genEvents).sample()
-        for (w <- List(1, 2, 4, 16)) {
+        for (w <- List(2)) {
           assertIncrementsEqualWhole(w, ves, seed)
         }
       }
 
+//>>>>>>>>> [windowSize=1, seed = -3431641283354444567L , events=257]
+// missing: ConflictingTags(UseCaseId(2),TagGroupId(12),NonEmptySet(CustomTextField(CustomField.Text.Id(9)), CustomTextField(CustomField.Text.Id(8))))
+//          ConflictingTags(UseCaseId(2),TagGroupId(22),NonEmptySet(CustomTextField(CustomField.Text.Id(9)), CustomTextField(CustomField.Text.Id(8))))
+//          ConflictingTags(UseCaseId(2),TagGroupId(30),NonEmptySet(CustomTextField(CustomField.Text.Id(9)), CustomTextField(CustomField.Text.Id(8))))
+//unwanted: ConflictingTags(UseCaseId(2),TagGroupId(12),NonEmptySet(CustomTextField(CustomField.Text.Id(8)), CustomTextField(CustomField.Text.Id(9))))
+//          ConflictingTags(UseCaseId(2),TagGroupId(22),NonEmptySet(CustomTextField(CustomField.Text.Id(8)), CustomTextField(CustomField.Text.Id(9))))
+//          ConflictingTags(UseCaseId(2),TagGroupId(30),NonEmptySet(CustomTextField(CustomField.Text.Id(8)), CustomTextField(CustomField.Text.Id(9))))
+//EventSeqSummary(
+//  customIssueTypes     = CUDR(C={}, U={}, D={}, R={})
+//  customFieldImpTypes  = CUDR(C={}, U={}, D={}, R={})
+//  customFieldTagTypes  = CUDR(C={}, U={}, D={}, R={})
+//  customFieldTextTypes = CUDR(C={}, U={}, D={}, R={})
+//  customReqTypes       = CUDR(C={}, U={}, D={}, R={})
+//  tagGroups            = CUDR(C={}, U={}, D={}, R={})
+//  applicableTags       = CUDR(C={}, U={}, D={}, R={})
+//  reqCodeGroups        = CUDR(C={}, U={}, D={}, R={})
+//  staticFields         = CUDR(C={}, U={}, D={}, R={})
+//  genericReqs          = CUDR(C={}, U={5}, D={}, R={})
+//  useCasesExclSteps    = CUDR(C={}, U={}, D={}, R={})
+//  useCaseSteps         = CUDR(C={}, U={}, D={}, R={})
+//  apReqCodes           = false
+//  contentLiveDeps      = false ){
+//  hasTags              = false }
+//
+//257: ReqFieldCustomTextSet(GenericReqId(5),CustomField.Text.Id(8),Vector(Issue(CustomIssueTypeId(3),Vector(EmailAddress($gX3quQQB%@*sd.i44cfliN.cDc6c%Ma1waG_.9Uv.lHL ), MathTeX(l P M *
+
+//      * - test(3363882791775821664L)
       * - test()
       * - test()
       * - test()
@@ -68,7 +118,17 @@ object IncrementalDetectionTest extends TestSuite {
       * - test()
       * - test()
       * - test()
-      * - test()
+    }
+  }
+
+  override def utestAfterAll(): Unit = {
+    if (compareIncTime) {
+      val samples = fullDurs.size
+      val List(fullTotal, incTotal) = List(fullDurs, incDurs).map(_.foldLeft(Duration.ZERO)(_ plus _))
+      val List(fullAvg, incAvg)     = List(fullTotal, incTotal).map(_.dividedBy(samples))
+
+      logCmpDur("AVERAGE", full = fullAvg, inc = incAvg)
+      logCmpDur("TOTAL", full = fullTotal, inc = incTotal)
     }
   }
 }
