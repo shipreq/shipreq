2532769803a28a2efe014dfad3238b72be358211
David Barri <japgolly@gmail.com>
2019-09-10 09:12:00 +1000

    Use new codecs in ssr

--- Code/webapp-ssr/shared/src/main/scala/shipreq/webapp/ssr/SsrShared.scala
+++ Code/webapp-ssr/shared/src/main/scala/shipreq/webapp/ssr/SsrShared.scala
@@ -1,5 +1,10 @@
 package shipreq.webapp.ssr
 
+import boopickle.DefaultBasic._
+import shipreq.webapp.base.user.Username
+import shipreq.webapp.base.data.Project
+import shipreq.webapp.base.protocol.binary.v1.BaseData._
+
 /** Values herein are all names of top-level functions in [SsrJs],
   * that can be called from [SsrInterpreter] on the JVM.
   */
@@ -10,19 +15,32 @@ object SsrJsFunctionManifest {
 }
 
 object SsrSharedData {
-  import shipreq.webapp.base.user.Username
-  import shipreq.webapp.base.data.Project
-  import shipreq.webapp.base.protocol.BinCodecGeneric._
-  import shipreq.webapp.base.protocol.BinCodecUser._
-  import shipreq.webapp.base.protocol.BoopickleMacros.pickleCaseClass
 
   type PublicInitData = shipreq.webapp.client.public.PublicSpaEntryPoint.InitData
   val  PublicInitData = shipreq.webapp.client.public.PublicSpaEntryPoint.InitData
 
+  // -------------------------------------------------------------------------------------------------------------------
+
   final case class HomeSpaLoaderData(username: Username)
-  implicit val picklerHomeSpaLoaderData = pickleCaseClass[HomeSpaLoaderData]
+
+  implicit val picklerHomeSpaLoaderData: Pickler[HomeSpaLoaderData] =
+    transformPickler(HomeSpaLoaderData.apply)(_.username)
+
+  // -------------------------------------------------------------------------------------------------------------------
 
   final case class ProjectSpaLoaderData(username: Username, projectName: Project.Name)
-  implicit val picklerProjectSpaLoaderData = pickleCaseClass[ProjectSpaLoaderData]
+
+  implicit val picklerProjectSpaLoaderData: Pickler[ProjectSpaLoaderData] =
+    new Pickler[ProjectSpaLoaderData] {
+      override def pickle(a: ProjectSpaLoaderData)(implicit state: PickleState): Unit = {
+        state.pickle(a.username)
+        state.pickle(a.projectName)
+      }
+      override def unpickle(implicit state: UnpickleState): ProjectSpaLoaderData = {
+        val username    = state.unpickle[Username]
+        val projectName = state.unpickle[Project.Name]
+        ProjectSpaLoaderData(username, projectName)
+      }
+    }
 
 }
