ecb045462d2da61adde88f7bc1a5478232cc3fe5
David Barri <japgolly@gmail.com>
2019-10-19 13:25:37 +1100

    Terraform: minor refactor

--- Infrastructure/terraform/dev/main.tf
+++ Infrastructure/terraform/dev/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.32"
+}
+
 module "shipreq" {
   source = "../modules/shipreq-env"
 
@@ -6,8 +11,8 @@ module "shipreq" {
     aws.ecr = aws.ap-southeast-2
   }
 
-  name               = "dev"
   env                = "dev"
+  name               = "Dev"
   vpc_ip_prefix      = "10.0"
   availability_zone  = "us-east-2b"
   bastion_public_key = file("key-bastion.rsa.pub")
@@ -18,4 +23,22 @@ module "shipreq" {
 
 output "bastion_host" {
   value = module.shipreq.bastion_host
+}
+
+####################################################################################################
+
+provider "aws" {
+  alias   = "ap-southeast-2"
+  region  = "ap-southeast-2"
+  version = "~> 2.32"
+}
+
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    bucket = "shipreq-terraform-state"
+    key    = "env-dev.tfstate"
+    region = "ap-southeast-2"
+  }
 }
