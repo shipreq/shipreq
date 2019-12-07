96a9771d7ec2378bd9c42324169adaf216776be5
David Barri <japgolly@gmail.com>
2019-12-07 20:38:04 +1100

    Prod Terraform: minor reorg & only encrypt secrets

---
+++ Infrastructure/terraform/prod/versions.tf
@@ -0,0 +1,7 @@
+locals {
+  versions = {
+    nat     = "git-d3fe30d8cced3649a16ecd3fb424861de10e28d0"
+    ops     = "git-d3fe30d8cced3649a16ecd3fb424861de10e28d0"
+    shipreq = "git-b5d9399046dfcffc5ca6e6d74108ea97f7645932"
+  }
+}
