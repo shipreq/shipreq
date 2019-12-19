fb03fc5f8e96989f1c59ade05b4e8707192f4500
David Barri <japgolly@gmail.com>
2019-12-19 11:42:59 +1100

    Update LogField to support MDC

--- Code/base-util/jvm/src/main/scala/shipreq/base/util/log/LogField.scala
+++ Code/base-util/jvm/src/main/scala/shipreq/base/util/log/LogField.scala
@@ -5,30 +5,47 @@ import io.circe.Encoder
 import io.circe.syntax._
 import japgolly.univeq._
 import java.time.Duration
+import java.util.UUID
 import net.logstash.logback.argument.StructuredArgument
 
-final class LogField[-A] private[LogField] (val key: String,
-                                            val fieldType: LogField.Type,
-                                            create: (String, A) => StructuredArgument) {
+sealed abstract class LogField[+T <: LogField.Type, @specialized(Long, Boolean) -A] private[log] (final val key: String) { self =>
 
-  LogField.Registry.register(key, fieldType)
+  val fieldType: T
+  protected def conv: A => fieldType.Input
 
   def apply(a: A): StructuredArgument =
-    create(key, a)
+    fieldType.create(key, conv(a))
 
-  private def copy[B](create2: (String, B) => StructuredArgument): LogField[B] =
-    new LogField(key, fieldType, create2)
+  def contramap[B](f: B => A): LogField[T, B] =
+    new LogField[T, B](key) {
+      override val fieldType: self.fieldType.type = self.fieldType
+      override protected def conv: B => fieldType.Input = self.conv compose f
+    }
 
-  def contramap[B](f: B => A): LogField[B] =
-    copy((k, b) => create(k, f(b)))
+  def optional(o: Option[A]): StructuredArgument =
+    o.fold(LogField.emptyArg)(apply)
 
-  def optional: LogField[Option[A]] =
-    copy((k, o) => o.fold(LogField.emptyArg)(create(k, _)))
+  def mdcUnsafePut(value: A)(implicit ev: T <:< LogField.Text.type): Unit = {
+    val _ = ev
+    val str = conv(value).asInstanceOf[LogField.Text.Input]
+    org.slf4j.MDC.put(key, str)
+  }
 }
 
 object LogField {
 
-  sealed trait Type
+  protected[LogField] def apply(key: String, t: Type): LogField[t.type, t.Input] = {
+    LogField.Registry.register(key, t)
+    new LogField[t.type, t.Input](key) {
+      override val fieldType: t.type = t
+      override protected def conv: t.Input => t.Input = i => i
+    }
+  }
+
+  sealed trait Type {
+    type Input
+    val create: (String, Input) => StructuredArgument
+  }
 
   private def creator[A](write: (JsonGenerator, A) => Unit): (String, A) => StructuredArgument =
     (key, value) =>
@@ -41,22 +58,27 @@ object LogField {
     }
 
   case object Text extends Type {
-    private val create = creator[String](_.writeString(_))
+    override type Input = String
+    override val create = creator[Input](_.writeString(_))
 
-    def apply(key: String): LogField[String] =
-      new LogField(key, this, create)
+    def apply(key: String): LogField[Text.type, String] =
+      LogField(key, this)
 
-    def json[A: Encoder](key: String): LogField[A] =
-      Text(key).contramap(_.asJson.noSpaces)
+    def json[A: Encoder](key: String): LogField[Text.type, A] =
+      apply(key).contramap(_.asJson.noSpaces)
+
+    def uuid(key: String): LogField[Text.type, UUID] =
+      apply(key).contramap(_.toString)
   }
 
   case object Long extends Type {
-    private val create = creator[Long](_.writeNumber(_))
+    override type Input = Long
+    override val create = creator[Input](_.writeNumber(_))
 
-    def apply(key: String): LogField[Long] =
-      new LogField(key, this, create)
+    def apply(key: String): LogField[Long.type, Long] =
+      LogField(key, this)
 
-    def durationMs(key: String): LogField[Duration] =
+    def durationMs(key: String): LogField[Long.type, Duration] =
       Long(key).contramap { d =>
         try
           d.toMillis
@@ -67,16 +89,20 @@ object LogField {
   }
 
   case object Bool extends Type {
-    private val create = creator[Boolean](_.writeBoolean(_))
+    override type Input = Boolean
+    override val create = creator[Input](_.writeBoolean(_))
 
-    def apply(key: String): LogField[Boolean] =
-      new LogField(key, this, create)
+    def apply(key: String): LogField[Bool.type, Boolean] =
+      LogField(key, this)
   }
 
   /** Unsafe because once ElasticSearch sees a certain type at a certain path, it tries to hold all future data to the
     * same expectation. This is safe so long as the JSON encoder always generates the same types at the same paths.
     */
   final case class UnsafeJson[A](encoder: Encoder[A]) extends Type {
+    override type Input = A
+    override val create = creator[Input]((g, a) => g.writeRawValue(a.asJson(encoder).noSpaces))
+
     override def equals(obj: Any) = obj match {
       case a: UnsafeJson[_] => encoder eq a.encoder
       case _                => false
@@ -84,19 +110,8 @@ object LogField {
   }
 
   object UnsafeJson {
-    def apply[A: Encoder](key: String): LogField[A] =
-      new LogField(
-        key,
-        new UnsafeJson(Encoder[A]),
-        (k, a) =>
-          new StructuredArgument {
-            override def writeTo(g: JsonGenerator): Unit = {
-              g.writeFieldName(k)
-              g.writeRawValue(a.asJson.noSpaces)
-            }
-            override def toString = "" + a
-        }
-      )
+    def apply[A: Encoder](key: String): LogField[UnsafeJson[A], A] =
+      LogField(key, new UnsafeJson(Encoder[A]))
   }
 
   // ===================================================================================================================
