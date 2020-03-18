6c3dd341b00b58ddadae2c4e80d4ed670c83f3c7
David Barri <japgolly@gmail.com>
2020-03-18 12:39:09 +1100

    Use real ShipReq events for EventBM

--- Code/benchmark/jvm/src/main/scala/shipreq/benchmark/SampleData.scala
+++ Code/benchmark/jvm/src/main/scala/shipreq/benchmark/SampleData.scala
@@ -3,10 +3,11 @@ package shipreq.benchmark
 import io.circe.Json
 import io.circe.parser.parse
 import scala.io.{Codec, Source}
-import shipreq.base.test.BaseTestUtil._
 import shipreq.webapp.base.data.Project
-import shipreq.webapp.base.event.Event
+import shipreq.webapp.base.event.{Event, VerifiedEvent}
 import shipreq.webapp.base.protocol.json.v1.Events.decoderEvent
+import shipreq.webapp.base.protocol.json.v1.PostEvents.decoderVerifiedEvent
+import shipreq.webapp.base.test.WebappTestUtil._
 import shipreq.benchmark.data._
 
 object SampleData {
@@ -26,4 +27,9 @@ object SampleData {
     loadJsonFromResource("shipreq-events-1000.json")
     .as[Vector[Event]]
     .needRight
+
+  lazy val project3Events: Vector[VerifiedEvent] =
+    loadJsonFromResource("project-3.json")
+      .as[Vector[VerifiedEvent]]
+      .needRight
 }
