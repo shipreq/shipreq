57edca8677449e85939e9422321ec9327a837406
David Barri <japgolly@gmail.com>
2020-04-19 20:24:09 +1000

    Add shipreq-events-2000.json

--- Code/benchmark/shared/src/main/scala/shipreq/benchmark/SampleDataManifest.scala
+++ Code/benchmark/shared/src/main/scala/shipreq/benchmark/SampleDataManifest.scala
@@ -3,9 +3,16 @@ package shipreq.benchmark
 trait SampleDataManifest[D] {
   protected def load(filename: String): D
 
-  lazy val `1000` : D = load("shipreq-events-1000.json")
+  lazy val  `1000`: D = load("shipreq-events-1000.json")
+  lazy val  `2000`: D = load("shipreq-events-2000.json")
   lazy val `10000`: D = load("shipreq-events-10000.json")
 
   lazy val all: Vector[D] =
-    Vector(`1000`, `10000`)
+    Vector(`1000`, `2000`, `10000`)
+
+  val byName: String => D = {
+    case  "1000" =>  `1000`
+    case  "2000" =>  `2000`
+    case "10000" => `10000`
+  }
 }
