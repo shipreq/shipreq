2b105451b988730a37e165a28790002b0d4f9961
David Barri <japgolly@gmail.com>
2020-04-20 18:08:51 +1000

    Run JS BMs for 60 sec each

--- Code/benchmark/js/src/main/scala/shipreq/benchmark/Main.scala
+++ Code/benchmark/js/src/main/scala/shipreq/benchmark/Main.scala
@@ -1,9 +1,11 @@
 package shipreq.benchmark
 
+import japgolly.scalajs.benchmark.engine.Options
 import japgolly.scalajs.benchmark.gui._
 import japgolly.scalajs.react.extra.router.BaseUrl
 import org.scalajs.dom
 import org.scalajs.dom.document
+import scala.concurrent.duration._
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 object Main {
@@ -14,9 +16,14 @@ object Main {
     val body    = document getElementById "body"
     val baseUrl = BaseUrl(dom.window.location.href.takeWhile(_ != '#'))
 
+    val options: Options =
+      Options.Default.copy(
+        maxTime = 60.seconds,
+      )
+
     val async =
       for (bd <- BenchmarkData.load) yield
-        BenchmarkGUI.renderMenu(body, baseUrl = baseUrl)(
+        BenchmarkGUI.renderMenu(body, baseUrl = baseUrl, options = options)(
           ApplyEventBM         (bd).guiSuite,
           BinarySerialisationBM(bd).guiSuite,
         )
