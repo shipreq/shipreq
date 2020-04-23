08340a3bc546eef9e124d98d1eb4fa786fdb222b
David Barri <japgolly@gmail.com>
2020-04-23 20:48:13 +1000

    Add BM results

--- Code/benchmark/jvm/src/main/scala/shipreq/benchmark/AtomScanBM.scala
+++ Code/benchmark/jvm/src/main/scala/shipreq/benchmark/AtomScanBM.scala
@@ -7,13 +7,11 @@ import shipreq.webapp.base.data.derivation.AtomScan
 import shipreq.webapp.sampledata.SampleData
 
 /*
-> jmh:run -wi 1 -i 1 -f 1 AtomScanBM
 
-[info] Benchmark            (events)  Mode  Cnt  Score   Error  Units
-[info] AtomScanBM.atomScan      1000  avgt       0.511          ms/op
-[info] AtomScanBM.atomScan      2000  avgt       0.821          ms/op
-[info] AtomScanBM.atomScan      4000  avgt       1.617          ms/op
-[info] AtomScanBM.atomScan     10000  avgt       3.635          ms/op
+> jmh:run -wi 1 -i 3 -f 1 AtomScanBM
+
+[info] AtomScanBM.atomScan     10000  avgt    3  2.333 ± 0.462  ms/op
+
  */
 
 @State(Scope.Benchmark)
