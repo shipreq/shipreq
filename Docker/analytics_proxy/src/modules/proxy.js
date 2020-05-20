f2ba08e86f7f3b5b603cd7b3b2fcca5e1c2ad334
David Barri <japgolly@gmail.com>
2020-05-20 18:36:47 +1000

    Update analytics_proxy

--- Docker/analytics_proxy/src/modules/proxy.js
+++ Docker/analytics_proxy/src/modules/proxy.js
@@ -16,12 +16,12 @@ const maskRegex = new RegExp(
     "gi"
 );
 const replaceDomainsForHost = (host) => (match, pos, str) => {
-    const escapedSlashes = str[pos - 2] === "\\" && str[pos - 2] === "/"
+    const escapedSlashes = str[pos - 2] === "\\" && str[pos - 2] === "/" // wat?
     const r = `${
         escapedSlashes
             ? (config.proxyDomain || host).replace(/\//g, "\\/") + "\\"
             : (config.proxyDomain || host)
-    }/${ match }`;
+    }${ config.strippedPath }/${ match }`;
     return r;
 };
 
@@ -37,19 +37,20 @@ export function createDefaultProxy (targetDomain, proxyOptionsOverride = {}) {
                 ? proxyOptionsOverride["proxyReqOptDecorator"](proxyRequest, originalRequest)
                 : proxyRequest;
         },
-        userResHeaderDecorator: (headers, { headers: { host } }) => {
-            if (headers.location) {
+        userResHeaderDecorator: (proxyHeaders, origninalHeaders) => {
+            const { headers: { host } } = origninalHeaders;
+            if (proxyHeaders.location) {
                 if (config.proxy.specialContentReplace[servername]) { // Keep before other replacements
                     const replacements = config.proxy.specialContentReplace[servername];
                     for (const r of replacements) {
-                        headers.location = headers.location.replace(r.regex, r.replace);
+                        proxyHeaders.location = proxyHeaders.location.replace(r.regex, r.replace);
                     }
                 }
-                headers.location = headers.location
+                proxyHeaders.location = proxyHeaders.location
                     .replace(replaceDomainRegex, replaceDomainsForHost(host))
                     .replace(maskRegex, match => mask(match));
             }
-            return headers;
+            return proxyHeaders;
         },
         userResDecorator: (_, proxyResData, { headers: { host } }) => {
             if (_.req.res && _.req.res.client && _.req.res.client.servername) {
@@ -84,8 +85,9 @@ export function createDefaultProxy (targetDomain, proxyOptionsOverride = {}) {
             ) {
 
                 const parsedUrl = url.parse(unmasked);
-                const clientIp = req.headers["x-forwarded-for"]
-                    ? req.headers["x-forwarded-for"].split(/,\s?/g)[0]
+                const pverwrittenIp = req.headers["x-forwarded-for"] || req.headers["x-real-ip"]; // TODO: || req.connection.remoteAddress; // in case no proxy is used (dedicated domain)
+                const clientIp = pverwrittenIp
+                    ? pverwrittenIp.split(/,\s?/g)[0]
                     : req.connection.remoteAddress.split(":").pop();
                 const encodedIp = encodeURIComponent(clientIp);
 
@@ -104,7 +106,7 @@ export function createDefaultProxy (targetDomain, proxyOptionsOverride = {}) {
                 ? proxyOptionsOverride["proxyReqPathResolver"](req, unmasked)
                 : unmasked;
 
-            info(`Proxied: ${ servername }${ finalPath }`);
+            info(`proxied: ${ servername }${ finalPath }`);
 
             return finalPath;
 
