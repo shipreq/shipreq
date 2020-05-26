1da409b5bdb8547717f050ffbd42c4eba7f68de0
David Barri <japgolly@gmail.com>
2020-05-26 13:17:24 +1000

    Add comments to WebWorker code

--- Code/webapp-client-ww-api/src/main/scala/shipreq/webapp/client/ww/api/Settings.scala
+++ Code/webapp-client-ww-api/src/main/scala/shipreq/webapp/client/ww/api/Settings.scala
@@ -4,11 +4,12 @@ import org.scalajs.dom.Transferable
 import scala.scalajs.js
 import Protocol._
 
+// Inherited by both Client and Server
 trait Settings {
   final protected val onError = OnError.Console
   final val codec: Codec.Binary.type = Codec.Binary
 
-  protected def transferables(e: codec.Encoded): js.UndefOr[js.Array[Transferable]] =
+  // https://developers.google.com/web/updates/2011/12/Transferable-Objects-Lightning-Fast
+  final protected def transferables(e: codec.Encoded): js.UndefOr[js.Array[Transferable]] =
     js.Array(e: Transferable)
 }
-
