69bbb7be4ed58a8f5f55c2e438b5819c75084876
David Barri <japgolly@gmail.com>
2020-06-05 14:23:04 +1000

    Add GeneticEvolution

---
+++ Code/base-util/shared/src/main/scala/shipreq/base/util/algorithm/GeneticEvolution.scala
@@ -0,0 +1,247 @@
+package shipreq.base.util.algorithm
+
+import java.util.{Comparator, Random}
+import scala.annotation.tailrec
+
+object GeneticEvolution {
+
+  object Binary {
+    import MutableLargeBitSet.BitsPerBlock
+
+    type Chromosome = MutableLargeBitSet
+
+    private[algorithm] type Population = Array[Chromosome]
+
+    /**
+     * @param fitness Must be ≥ 0 where 0 is perfection
+     * @param goal A chromosone with a fitness ≤ goal is considered success
+     */
+    def apply(bits   : Int,
+              fitness: Chromosome => Double,
+              goal   : Double,
+              config : Config): Chromosome = {
+
+      val isSmall = bits <= 16
+      val domain = (1 << bits) - 1
+
+      if (bits <= 0)
+        MutableLargeBitSet.empty
+
+      else if (isSmall && domain <= config.popSize) {
+        val c = MutableLargeBitSet(bits)
+        var i = domain
+        var bestFitness = Double.PositiveInfinity
+        var bestValue = -1
+        while (i > 0) {
+          i -= 1
+          c.data(0) = i
+          val f = fitness(c)
+          if (f < bestFitness) {
+            bestFitness = f
+            bestValue = i
+          }
+        }
+        c.data(0) = bestValue
+        c
+
+      } else
+        solveViaEvolution(bits, fitness, goal, config)
+    }
+
+    private[algorithm] def solveViaEvolution(bits   : Int,
+                                             fitness: Chromosome => Double,
+                                             goal   : Double,
+                                             config : Config): Chromosome = {
+      assert(config.popSize >= 4)
+
+      val popSize    = config.popSize
+      val rng        = new Random(0)
+      val fitnesses  = Array.fill(popSize)(new Fitness)
+      val select     = selection(fitnesses)
+      var fitnessSum = 0.0
+      var i          = 0
+
+      val rndInt: RndInt =
+        rng.nextInt(_)
+
+      val doCrossover = crossover(bits, rndInt)
+
+      def newChromosome(): Chromosome =
+        MutableLargeBitSet(bits).randomise(rng)
+
+      def newPopulation(): Population =
+        Array.fill(popSize)(newChromosome())
+
+      val shouldMutate: () => Boolean =
+        if (config.mutationRate >= 1)
+          () => true
+        else if (config.mutationRate <= 0)
+          () => false
+        else
+          () => rng.nextDouble() <= config.mutationRate
+
+      @tailrec def run(generation: Int, population: Population): Chromosome = {
+
+        // Calculate fitness of each chromosone
+        fitnessSum = 0.0
+        i = fitnesses.length
+        while (i > 0) {
+          i -= 1
+          val chromosone = population(i)
+          val x = fitnesses(i)
+          val f = fitness(chromosone)
+          x.popIdx = i
+          x.fitness = f
+          fitnessSum += f
+        }
+
+        // Rank fitness of each chromosone
+        if (fitnessSum > 0) {
+          java.util.Arrays.sort(fitnesses, fitnessCmp)
+          var acc = 0.0
+          i = fitnesses.length
+          while (i > 0) {
+            i -= 1
+            val f = fitnesses(i)
+            acc += f.fitness / fitnessSum
+            f.score = acc
+          }
+        } else {
+          // clear scores
+          i = fitnesses.length
+          while (i > 0) {
+            i -= 1
+            val f = fitnesses(i)
+            f.score = 0
+          }
+        }
+
+        // Print progress
+//        println(s"---------------- Gen ${generation} ----------------")
+//        for (f <- fitnesses.iterator.take(3)) {
+//          val c = population(f.popIdx)
+//          printf("[%s] fitness = %012.10f, score = %012.10f\n", c.toBinaryStr, f.fitness, f.score)
+//        }
+
+        // Check for success
+        val best = fitnesses(0)
+        if (best.fitness <= goal || generation >= config.maxGens)
+          population(best.popIdx)
+
+        else {
+
+          // Evolve
+          val nextGeneration: Population =
+            Array.tabulate(popSize) { n =>
+
+              // Elitism: retain the best few without modification
+              if (n < config.eliteSize) {
+                population(fitnesses(n).popIdx)
+
+              } else {
+
+                // Select parents
+                val f1 = select(rng.nextDouble(), -1)
+                val f2 = select(rng.nextDouble(), f1)
+                val c1 = population(fitnesses(f1).popIdx)
+                val c2 = population(fitnesses(f2).popIdx)
+
+                // Crossover
+                val c = doCrossover(c1, c2)
+
+                // Mutate
+                var i = bits
+                while (i > 0) {
+                  i -= 1
+                  if (shouldMutate())
+                    c.flipBit(i)
+                }
+
+                c
+              }
+            }
+
+          run(generation + 1, nextGeneration)
+        }
+      }
+
+      run(0, newPopulation())
+    }
+
+    private[algorithm] trait RndInt {
+      /**
+        * @param bound ≥ 0
+        * @return [0,bound)
+        */
+      def nextInt(bound: Int): Int
+    }
+
+    private[algorithm] def selection(fitnesses: Array[Fitness]): (Double, Int) => Int =
+      (p: Double, skipIdx: Int) => {
+        // TODO scale down p when skipIdx >= 0
+        @tailrec def go(i: Int): Int =
+          if (i == skipIdx)
+            go(i + 1)
+          else if (i == fitnesses.length) {
+            val j = i - 1
+            if (j == skipIdx) j - 1 else j
+          } else {
+            val s = fitnesses(i).score
+            if (s >= p)
+              go(i + 1)
+            else
+              i - 1
+          }
+        go(0).max(0)
+      }
+
+    private[algorithm] def crossover(bits: Int, rndInt: RndInt): (Chromosome, Chromosome) => Chromosome = {
+      val gaps  = bits - 1
+      val bound = gaps << 1
+      (aa, bb) => {
+        var a = aa
+        var b = bb
+        var c = rndInt.nextInt(bound)
+        if (c >= gaps) {
+          c -= gaps
+          a = bb
+          b = aa
+        }
+        val x = a.clone()
+        var i = a.data.length - 1
+        while (c >= BitsPerBlock) {
+          x.data(i) = b.data(i)
+          i -= 1
+          c -= BitsPerBlock
+        }
+        if (i >= 0) {
+          val mb = (2L << c) - 1
+          val ma = mb ^ -1
+          x.data(i) = (a.data(i) & ma) | (b.data(i) & mb)
+        }
+        x
+      }
+    }
+
+    final case class Config(popSize     : Int,
+                            eliteSize   : Int,
+                            maxGens     : Int,
+                            mutationRate: Double)
+
+    private[algorithm] final class Fitness {
+      var popIdx : Int    = _
+      var fitness: Double = _ // [0,∞) where 0 = perfection
+      var score  : Double = _ // [0,1] where 1 = 100% chance of selection
+    }
+
+    private val fitnessCmp: Comparator[Fitness] =
+      (x, y) =>
+        if (x.fitness == y.fitness)
+          0
+        else if (x.fitness < y.fitness)
+          -1
+        else
+          1
+  }
+
+}
---
+++ Code/base-util/shared/src/test/scala/shipreq/base/util/algorithm/GeneticEvolutionTest.scala
@@ -0,0 +1,157 @@
+package shipreq.base.util.algorithm
+
+import japgolly.microlibs.testutil.TestUtil._
+import sourcecode.Line
+import utest._
+
+object GeneticEvolutionTest extends TestSuite {
+  import GeneticEvolution.Binary._
+
+  private implicit def chromosomeFromString(binaryString: String): Chromosome =
+    MutableLargeBitSet.fromBinaryString(binaryString)
+
+  private implicit final class StringTestOps(private val self: String) extends AnyVal {
+    def c: Chromosome = self
+  }
+
+  private def assertChromosoneEq(name: String, a: Chromosome, e: Chromosome)(implicit l: Line): Unit =
+    assertEq(name, a.toBinaryStr, e.toBinaryStr)
+
+  private def assertChromosoneEq(a: Chromosome, e: Chromosome)(implicit l: Line): Unit =
+    assertEq(a.toBinaryStr, e.toBinaryStr)
+
+  private implicit def constRndInt(x: (Int, Int))(implicit l: Line): RndInt =
+    (bound: Int) => {
+      assertEq("RndInt.bound", bound, x._1)
+      x._2
+    }
+
+  private implicit def constRndInt2(x: Int): RndInt =
+    _ => x
+
+  private val L0 = "0000000000000000000000000000000000000000000000000000000000000000"
+  private val L1 = "1111111111111111111111111111111111111111111111111111111111111111"
+  private val L2 = "1010101010101010101010101010101010101010101010101010101010101010"
+  private val L3 = "0101010101010101010101010101010101010101010101010101010101010101"
+
+  private val LX = List(L0, L1, L2, L3)
+  private val LXX = for {a <- LX; b <- LX} yield a + b
+
+  override def tests = Tests {
+
+    "testHelpers" - {
+      def test(bin: String, genes: Long*)(implicit l: Line): Unit = {
+        val bits = bin.length
+        val c = MutableLargeBitSet.fromArray(bits, genes.toArray)
+        assertChromosoneEq("binary -> chromosone", bin: Chromosome, c)
+        assertEq("chromosone -> binary", c.toBinaryStr, bin)
+      }
+      "0" - test("0", 0)
+      "01" - test("01", 1)
+      "10" - test("10", 2)
+      "011" - test("011", 3)
+      "L0" - test(L0, 0)
+      "L1" - test(L1, -1)
+    }
+
+    "selection" - {
+      val fs = Array.tabulate(4){n => val f = new Fitness; f.popIdx = n+10; f}
+      val s = selection(fs)
+      fs(0).score = 1
+      fs(1).score = .7
+      fs(2).score = .5
+      fs(3).score = .1
+      "full" - {
+        val tests = List[(Double, Int)](
+          .05 -> 3,
+          .1 -> 3,
+          .15 -> 2,
+          .4 -> 2,
+          .5 -> 2,
+          .6 -> 1,
+          .7 -> 1,
+          .8 -> 0,
+          .9 -> 0,
+          1.0 -> 0,
+        )
+        for ((p, e) <- tests)
+          assertEq(s"p=$p", s(p, -1), e)
+      }
+    }
+
+    "crossover" - {
+      def test(a: String, b: String, rnd: RndInt, expect: Chromosome)(implicit l: Line): Unit = {
+        assertEq(a.length, b.length)
+        val bits = a.length
+        val actual = crossover(bits, rnd)(a, b)
+        def bound = (bits - 1) << 1
+        def desc = s"$bits/${rnd.nextInt(bound)}\n     a: $a\n     b: $b"
+        assertChromosoneEq(desc, actual, expect)
+      }
+
+      "2/0" - test("00", "11", 2 -> 0, "01")
+      "2/1" - test("00", "11", 2 -> 1, "10")
+
+      "4/0" - test("000", "111", 4 -> 0, "001")
+      "4/1" - test("000", "111", 4 -> 1, "011")
+      "4/2" - test("000", "111", 4 -> 2, "110")
+      "4/3" - test("000", "111", 4 -> 3, "100")
+
+      "whole" - {
+        for {
+          l <- MutableLargeBitSetTest.bitsLens.filter(_ <= 127).filter(_ >= 1)
+          a <- LXX
+          b <- LXX
+        } {
+          val gaps = 127
+          val c = l - 1
+          val e = a.dropRight(l) + b.takeRight(l)
+          test(a, b, c, e)
+          test(b, a, c + gaps, e)
+        }
+      }
+    }
+
+    "run" - {
+      assertEq(L0.c.fitnessToGoal(L0, L0.length), 0)
+//      assertEq(L0.c.fitnessToGoal(L1, L0.length), 1)
+//      assertEqWithTolerance(s"101${L0}010".c.fitnessToGoal(s"011${L0}011", 70), 3 / 70.0, 0.00000001)
+//      assertEqWithTolerance("111001110".c.fitnessToGoal("000000000", 9), 6 / 9.0)
+
+      assertEq("111".c.bit(0), true)
+      assertEq("111".c.bit(1), true)
+      assertEq("111".c.bit(2), true)
+      assertEq("000".c.bit(0), false)
+      assertEq("000".c.bit(1), false)
+      assertEq("000".c.bit(2), false)
+      assertEq("10".c.bit(0), false)
+      assertEq("10".c.bit(1), true)
+      assertEq("01".c.bit(0), true)
+      assertEq("01".c.bit(1), false)
+
+      def run(bits  : Int,
+               target: Chromosome,
+               config: Config): Chromosome = {
+        val bitsD = bits.toDouble
+        GeneticEvolution.Binary.solveViaEvolution(
+          bits    = bits,
+          fitness = _.fitnessToGoal(target, bits, bitsD),
+          goal    = 0,
+          config  = config,
+        )
+      }
+
+      //      val goal = "10001000"
+      val goal = "101" + L0
+      val bits = goal.length
+      val config = Config(
+        popSize      = 128,
+        eliteSize    = 64,
+        maxGens      = 10000,
+        mutationRate = .01,
+      )
+      val result = run(bits, goal, config)
+      assertChromosoneEq(result, goal)
+    }
+  }
+}
