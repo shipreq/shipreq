96650aee0fe4bb88ce771adf445fc18e49c0de52
David Barri <japgolly@gmail.com>
2020-07-12 20:57:48 +1000

    Reduce ECR policy latest{32 => 20}

--- Infrastructure/terraform/global/ecr_lifecycle_policies.tf
+++ Infrastructure/terraform/global/ecr_lifecycle_policies.tf
@@ -19,16 +19,16 @@ locals {
 }
 EOB
 
-  ecr_lifecycle_policy_latest32 = <<EOB
+  ecr_lifecycle_policy_latest20 = <<EOB
 {
   "rules": [
     {
       "rulePriority": 1,
-      "description": "Only keep latest 32 images",
+      "description": "Only keep latest 20 images",
       "selection": {
         "tagStatus": "any",
         "countType": "imageCountMoreThan",
-        "countNumber": 32
+        "countNumber": 20
       },
       "action": {
         "type": "expire"
@@ -41,7 +41,7 @@ EOB
 
 resource "aws_ecr_lifecycle_policy" "analytics_proxy" {
   repository = aws_ecr_repository.analytics_proxy.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "base" {
@@ -51,65 +51,65 @@ resource "aws_ecr_lifecycle_policy" "base" {
 
 resource "aws_ecr_lifecycle_policy" "cadvisor" {
   repository = aws_ecr_repository.cadvisor.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "ecs_exporter" {
   repository = aws_ecr_repository.ecs_exporter.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "filebeat" {
   repository = aws_ecr_repository.filebeat.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "grafana" {
   repository = aws_ecr_repository.grafana.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "nat" {
   repository = aws_ecr_repository.nat.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "node_exporter" {
   repository = aws_ecr_repository.node_exporter.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "portal" {
   repository = aws_ecr_repository.portal.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "postgres_exporter" {
   repository = aws_ecr_repository.postgres_exporter.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "prometheus-biz" {
   repository = aws_ecr_repository.prometheus-biz.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "prometheus-tech" {
   repository = aws_ecr_repository.prometheus-tech.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "squid_exporter" {
   repository = aws_ecr_repository.squid_exporter.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "taskman" {
   repository = aws_ecr_repository.taskman.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
 
 resource "aws_ecr_lifecycle_policy" "webapp" {
   repository = aws_ecr_repository.webapp.name
-  policy     = local.ecr_lifecycle_policy_latest32
+  policy     = local.ecr_lifecycle_policy_latest20
 }
