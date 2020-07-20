80527fb4d69b6f53680ddb6dce85b7306374bad3
David Barri <japgolly@gmail.com>
2020-07-20 18:13:54 +1000

    Add manifest & favicon

--- Web/tech/gatsby-config.js
+++ Web/tech/gatsby-config.js
@@ -4,15 +4,29 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+ const manifest = {
+  resolve: `gatsby-plugin-manifest`,
+  options: {
+    name: "ShipReq tech blog",
+    start_url: "/",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
+    display: "browser",
+    icon: "src/images/shipreq-logo-only.svg",
+    crossOrigin: `use-credentials`,
+  },
+};
+
+const typography = {
+  resolve: `gatsby-plugin-typography`,
+  options: {
+    pathToConfigModule: `src/utils/typography`,
+  },
+};
+
 module.exports = {
   plugins: [
-
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-
+    manifest,
+    typography,
   ],
 }
