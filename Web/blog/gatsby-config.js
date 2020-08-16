bf1d011d8c17dff6f46242b1453c8d5b33125353
David Barri <japgolly@gmail.com>
2020-08-16 12:35:20 +1000

    Remove typography

--- Web/blog/gatsby-config.js
+++ Web/blog/gatsby-config.js
@@ -97,14 +97,6 @@ module.exports = {
 
     // =============================================================================================
     {
-      resolve: "gatsby-plugin-typography",
-      options: {
-        pathToConfigModule: "src/utils/typography",
-      },
-    },
-
-    // =============================================================================================
-    {
       resolve: `gatsby-plugin-feed`,
       options: {
         feeds: [
