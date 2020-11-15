abb512f5021b3fc2d8b8862412349b3bdea4e969
David Barri <japgolly@gmail.com>
2020-11-16 09:46:21 +1100

    Make IndexedDbCodec async and test with crypto - doesn't work
    IndexedDb itself is async but it doesn't allow anything else to be.
    See https://stackoverflow.com/a/35611053

--- Code/webapp-member-test/js/src/test/scala/shipreq/webapp/member/protocol/indexeddb/IndexedDbTest.scala
+++ Code/webapp-member-test/js/src/test/scala/shipreq/webapp/member/protocol/indexeddb/IndexedDbTest.scala
@@ -1,9 +1,15 @@
 package shipreq.webapp.member.protocol.indexeddb
 
+import japgolly.scalajs.react.AsyncCallback
+import shipreq.webapp.member.test.TestEncryption
 import shipreq.base.test.BaseTestUtil._
 import shipreq.base.test.Node.asyncTest
 import shipreq.webapp.member.test.TestIndexedDb
 import utest._
+import shipreq.webapp.base.protocol.binary.SafePickler
+import shipreq.webapp.member.project.data.Project
+import shipreq.webapp.member.protocol.binary.Compression
+import shipreq.webapp.member.test.project._
 
 object IndexedDbTest extends TestSuite {
   import IndexedDb._
@@ -20,7 +26,7 @@ object IndexedDbTest extends TestSuite {
         )
 
       for {
-        db      <- TestIndexedDb().open("blah")(openCallbacks)
+        db      <- TestIndexedDb().open("IndexedDbTest_basic")(openCallbacks)
 
         rw      <- db.transactionRW(store).asAsyncCallback
         storeRW <- rw.objectStore(store).asAsyncCallback
@@ -34,6 +40,56 @@ object IndexedDbTest extends TestSuite {
       } yield {
         assertEq(v1, Some("hello"))
         assertEq(v2, None)
+      }
+    }
+
+    "stack" - asyncTest {
+      import shipreq.webapp.member.project.protocol.binary.v1.Latest.picklerProject
+      import SampleProject8.project
+      import SafePickler.ConstructionHelperImplicits._
+      import TestEncryption.UnsafeTypes._
+
+      implicit val safePicklerProject: SafePickler[Project] =
+        picklerProject.asV1(0).withMagicNumbers(0x89827590, 0x8858F858)
+
+      val zip = Compression(3, addHeaders = false)
+
+      val dbName = "IndexedDbTest_stack"
+      val storeName = "s"
+      val key = 123
+
+      def openCallbacks(store: ObjectStoreDef[_]) =
+        TestIndexedDb.unusedOpenCallbacks.copy(
+          upgradeNeeded = _.db.createObjectStore(store).void.asAsyncCallback
+        )
+
+      for {
+        enc1     <- TestEncryption("a" * 32)
+//        enc2     <- TestEncryption("b" * 32)
+//        codec1    <- AsyncCallback.delay(IndexedDbCodec.default[Project](zip, enc1))
+        codec1    <- AsyncCallback.delay(IndexedDbCodec.binary.encrypt(enc1).pickle[Project])
+//        codec2    = IndexedDbCodec.default[Project](zip, enc2)
+        store1    <- AsyncCallback.delay(ObjectStoreDef(storeName)(codec1))
+//        store2    = ObjectStoreDef(storeName)(codec2)
+        db1      <- TestIndexedDb().open(dbName)(openCallbacks(store1))
+//        db2      <- TestIndexedDb().open(dbName)(openCallbacks(store2))
+
+        rw       <- db1.transactionRW(store1).asAsyncCallback.logResult(implicitly[sourcecode.Line].value.toString)
+        storeRW  <- rw.objectStore(store1).asAsyncCallback.logResult(implicitly[sourcecode.Line].value.toString)
+        _        <- storeRW.add(key, project).logResult(implicitly[sourcecode.Line].value.toString)
+        _        <- rw.completion.logResult(implicitly[sourcecode.Line].value.toString)
+
+        ro1      <- db1.transactionRO(store1).asAsyncCallback.logResult(implicitly[sourcecode.Line].value.toString)
+        storeRO1 <- ro1.objectStore(store1).asAsyncCallback.logResult(implicitly[sourcecode.Line].value.toString)
+        p1       <- storeRO1.get(key).logResult(implicitly[sourcecode.Line].value.toString)
+        _        <- ro1.completion.logResult(implicitly[sourcecode.Line].value.toString)
+
+//        ro2      <- db2.transactionRO(store2).asAsyncCallback.logAround("y")
+//        storeRO2 <- ro2.objectStore(store2).asAsyncCallback.logAround("z")
+//        p2       <- storeRO2.get(key).attempt
+        
+      } yield {
+        assertEq(p1, Some(project))
       }
     }
   }
--- Code/webapp-member/js/src/main/scala/shipreq/webapp/member/protocol/indexeddb/IndexedDbCodec.scala
+++ Code/webapp-member/js/src/main/scala/shipreq/webapp/member/protocol/indexeddb/IndexedDbCodec.scala
@@ -5,16 +5,17 @@ import scala.scalajs.js
 import scala.scalajs.js.typedarray.ArrayBuffer
 import shipreq.base.util.BinaryData
 import shipreq.base.util.JsExt._
+import shipreq.webapp.base.protocol.binary.SafePickler
 import shipreq.webapp.member.protocol.binary.{Compression, Encryption}
 
 final case class IndexedDbCodec[A](encode: A => AsyncCallback[js.Any],
                                    decode: Any => AsyncCallback[A]) {
 
   def xmap[B](onDecode: A => B)(onEncode: B => A): IndexedDbCodec[B] =
-    IndexedDbCodec[B](
-      encode = b => encode(onEncode(b)),
-      decode = d => decode(d).map(onDecode),
-    )
+    // Delegating to xmapAsync because decoding can fail and must be wrapped in AsyncCallback to be pure.
+    xmapAsync(
+      a => AsyncCallback.delay(onDecode(a)))(
+      b => AsyncCallback.delay(onEncode(b)))
 
   def xmapAsync[B](onDecode: A => AsyncCallback[B])(onEncode: B => AsyncCallback[A]): IndexedDbCodec[B] =
     IndexedDbCodec[B](
@@ -22,15 +23,16 @@ final case class IndexedDbCodec[A](encode: A => AsyncCallback[js.Any],
       decode = decode(_).flatMap(onDecode),
     )
 
-  def compress(c: Compression)(implicit ev: IndexedDbCodec[A] =:= IndexedDbCodec[BinaryData]): IndexedDbCodec[BinaryData] =
+  type ThisIsBinary = IndexedDbCodec[A] =:= IndexedDbCodec[BinaryData]
+
+  def compress(c: Compression)(implicit ev: ThisIsBinary): IndexedDbCodec[BinaryData] =
     ev(this).xmap(c.decompressOrThrow)(c.compress)
 
-  def encrypt(e: Encryption)(implicit ev: IndexedDbCodec[A] =:= IndexedDbCodec[BinaryData]): IndexedDbCodec[BinaryData] =
+  def encrypt(e: Encryption)(implicit ev: ThisIsBinary): IndexedDbCodec[BinaryData] =
     ev(this).xmapAsync(e.decrypt)(e.encrypt)
 
-//  TODO def pickle[B](implicit ev: IndexedDbCodec[A] =:= IndexedDbCodec[BinaryData]): IndexedDbCodec[B] = {
-//    val self = ev(this)
-//  }
+  def pickle[B](implicit pickler: SafePickler[B], ev: ThisIsBinary): IndexedDbCodec[B] =
+    ev(this).xmap(pickler.decodeOrThrow)(pickler.encode)
 }
 
 object IndexedDbCodec {
@@ -38,12 +40,7 @@ object IndexedDbCodec {
   val binary: IndexedDbCodec[BinaryData] =
     apply(
       encode = b => AsyncCallback.pure(b.unsafeArrayBuffer),
-      decode = d => AsyncCallback.delay(
-        d match {
-          case ab: ArrayBuffer => BinaryData.unsafeFromArrayBuffer(ab)
-          case x               => throw new RuntimeException("ArrayBuffer expected: " + x)
-        }
-      )
+      decode = d => AsyncCallback.delay(BinaryData.unsafeFromArrayBuffer(d.asInstanceOf[ArrayBuffer]))
     )
 
   lazy val string: IndexedDbCodec[String] =
@@ -56,4 +53,10 @@ object IndexedDbCodec {
         }
       )
     )
+
+  def default[A](c: Compression, e: Encryption)(implicit pickler: SafePickler[A]): IndexedDbCodec[A] =
+    binary
+      .encrypt(e) // Encryption is the very last step
+      .compress(c) // Here we compress the binary *before* encrypting
+      .pickle[A]
 }
