5e9d2f0a92448404c392955320946abcb0fb9a29
David Barri <japgolly@gmail.com>
2020-11-24 18:47:14 +1100

    Add frontend assets for web worker

---
+++ Code/frontend/src/js/ww.js
@@ -0,0 +1,10 @@
+// pako
+const Pako = require('pako/dist/pako.min')
+export { Pako }
+
+// base32768
+import * as B32768 from 'base32768/dist/es6/base32768'
+export { B32768 }
+
+// Paste the following into web console for quick testing.
+// console.log("G: ", {B32768, Pako})
