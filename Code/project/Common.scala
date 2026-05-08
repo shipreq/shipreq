6422d261fb0f3f311696b6e5231347d4b8009778
David Barri <japgolly@gmail.com>
2026-05-08 15:35:07 +1000

    Try `Tags.limitAll(1)`

--- Code/project/Common.scala
+++ Code/project/Common.scala
@@ -131,7 +131,7 @@ object Common {
       _.settings(
         Test / parallelExecution := false,
         Global / concurrentRestrictions += Tags.limit(Tags.Test, 1),
-        Global / concurrentRestrictions += Tags.limitAll(cores),
+        Global / concurrentRestrictions += Tags.limitAll(1),
       )
     else
       _.settings(
