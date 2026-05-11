167fb23a7566bb10a3ad1d9655af84a6a083db23
David Barri <japgolly@gmail.com>
2026-05-11 19:06:46 +1000

    Upgrade some deps

--- Code/project/Dependencies.scala
+++ Code/project/Dependencies.scala
@@ -6,7 +6,7 @@ import LibDependency._
 object Dependencies {
 
   object Akka {
-    private val mm = MultiModule.scala("com.typesafe.akka", "2.6.16")
+    private val mm = MultiModule.scala("com.typesafe.akka", "2.8.8")
     val actor   = mm("akka-actor") ++ mm("akka-slf4j")
     val testkit = mm("akka-testkit")
   }
@@ -47,7 +47,7 @@ object Dependencies {
     // Note: when changing this, make sure to also change:
     //   - :/Docker/dev-build_env/Dockerfile (the aur git sha)
     //   - :/Docker/shipreq-base/Dockerfile
-    val ver = "23.1.10"
+    val ver = "23.1.11"
 
     lazy val js =
       jvmOnly("org.graalvm.polyglot" %"polyglot" % ver) ++
@@ -222,7 +222,7 @@ object Dependencies {
   val utest       = jvmAndJs("com.github.japgolly.fork",         "utest",       "1.0.3")
 
   val commonsIo    = jvmOnly("org.apache.directory.studio" % "org.apache.commons.io" % "2.4")
-  val commonsText  = jvmOnly("org.apache.commons"          % "commons-text"          % "1.9")
+  val commonsText  = jvmOnly("org.apache.commons"          % "commons-text"          % "1.15.0")
   val flyway       = jvmOnly("org.flywaydb"                % "flyway-core"           % "8.0.5")
   val hikariCP     = jvmOnly("com.zaxxer"                  % "HikariCP"              % "7.0.2")
   val httpCore     = jvmOnly("org.apache.httpcomponents"   % "httpcore"              % "4.4.14")
@@ -231,9 +231,9 @@ object Dependencies {
   val postgresql   = jvmOnly("org.postgresql"              % "postgresql"            % "42.7.11")
   val redisson     = jvmOnly("org.redisson"                % "redisson"              % "3.16.3")
   val scaffeine    = jvmOnly("com.github.blemale"         %% "scaffeine"             % "4.0.2")
-  val scalaCheck   = jvmOnly("org.scalacheck"             %% "scalacheck"            % "1.15.4")
-  val scalaLogging = jvmOnly("com.typesafe.scala-logging" %% "scala-logging"         % "3.9.4")
-  val scalaXml     = jvmOnly("org.scala-lang.modules"     %% "scala-xml"             % "1.3.0")
+  val scalaCheck   = jvmOnly("org.scalacheck"             %% "scalacheck"            % "1.19.0")
+  val scalaLogging = jvmOnly("com.typesafe.scala-logging" %% "scala-logging"         % "3.9.6")
+  val scalaXml     = jvmOnly("org.scala-lang.modules"     %% "scala-xml"             % "1.3.1")
   val scalaz       = jvmOnly("org.scalaz"                 %% "scalaz-core"           % "7.3.8") // only used for scalaz.Heap
 
   val scalajsBenchmark = jsOnly("com.github.japgolly.scalajs-benchmark" %% "benchmark"         % "0.10.0")
